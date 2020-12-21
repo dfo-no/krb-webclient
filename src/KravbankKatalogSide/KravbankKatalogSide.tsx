bdced3653f435d374f1cc23f9ba225c325098f8c
imed-computas <73219353+imed-computas@users.noreply.github.com>
2020-12-21 12:26:16 +0100

    Krb 17 (#9)
    * adding search on katalog items
    
    * SearchBar props
    
    * small adjustment to css, and conditional rendering of component
    
    Co-authored-by: Trond Albinussen <tal@computas.com>

--- src/KravbankKatalogSide/KravbankKatalogSide.tsx
+++ src/KravbankKatalogSide/KravbankKatalogSide.tsx
@@ -1,39 +1,17 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import styles from './KravbankKatalogSide.module.scss';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import data from '../data/katalog.json';
 import { Katalog } from '../models/Katalog';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { Kravbank } from '../models/Kravbank';
-
-const renderKatalogItems = (katalogItems: Kravbank[]) => {
-  return katalogItems.map((kravbank, index) => (
-    <Draggable key={kravbank.id} draggableId={kravbank.id} index={index}>
-      {(provided) => (
-        <div
-          className={styles.katalogitem}
-          key={kravbank.id}
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <p>{kravbank.tittel}</p>
-          <Link to={'/edit/' + kravbank.id}>
-            <button className={styles.editbutton} type="button">
-              Rediger
-            </button>
-          </Link>
-        </div>
-      )}
-    </Draggable>
-  ));
-};
+import SearchBar from '../SearchBar/SearchBar';
 
 export default function KravbankKatalogSide(this: any): ReactElement {
   const [katalog, setKatalog] = useState<Katalog>({});
+  const [loading, setLoading] = useState<boolean>(true);
   const defaultkatalogitem = [
     {
-      id: '',
+      id: 'default',
       tittel: '',
       beskrivelse: ''
     }
@@ -51,22 +29,16 @@ export default function KravbankKatalogSide(this: any): ReactElement {
   };
 
   useEffect(() => {
-    if (Object.values(katalog).length === 0) fetchKatalog();
-  });
+    if (Object.values(katalog).length === 0) {
+      fetchKatalog();
+    } else {
+      setLoading(false);
+    }
+  }, [katalog]);
 
   useEffect(() => {
     setKatalogItems(Object.values(katalog));
   }, [katalog]);
-
-  const handleOnDragEnd = (result: any) => {
-    if (!result.destination) return;
-    const items = katalogitems;
-    console.log(items);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setKatalogItems(items);
-  };
 
   return (
     <div className={styles.container}>
@@ -77,22 +49,8 @@ export default function KravbankKatalogSide(this: any): ReactElement {
       >
         Opprett Kravbank
       </button>
-      <div className={styles.katalogcontainer}>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="katalogitems">
-            {(provided) => (
-              <ul
-                className="katalogitems"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {renderKatalogItems(katalogitems)}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
+      <div className={styles.katalogcontainer}></div>
+      {loading ? <div></div> : <SearchBar list={katalogitems} />}
     </div>
   );
 }
---
+++ src/SearchBar/SearchBar.tsx
@@ -0,0 +1,53 @@
+import React, { ReactElement, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Kravbank } from '../models/Kravbank';
+
+import styles from './SearchBar.module.scss';
+
+const displaylist = (list: Kravbank[]) => {
+  return list.map((kravbank: Kravbank) => {
+    if (kravbank.tittel) {
+      return (
+        <div className={styles.katalogitem} key={kravbank.id}>
+          <p>{kravbank.tittel}</p>
+          <Link to={'/edit/' + kravbank.id}>
+            <button className={styles.editbutton} type="button">
+              Rediger
+            </button>
+          </Link>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  });
+};
+
+interface SearchBarProps {
+  list: Kravbank[];
+}
+
+export default function SearchBar(props: SearchBarProps): ReactElement {
+  const [input, setInput] = useState('');
+  const [searchList, setSearchList] = useState(props.list);
+
+  const updateInput = async (input: any) => {
+    const filtered = props.list.filter((element) => {
+      return element.tittel.toLowerCase().includes(input.toLowerCase());
+    });
+    setInput(input);
+    setSearchList(filtered);
+  };
+
+  return (
+    <div>
+      <input
+        className={styles.BarStyling}
+        value={input}
+        placeholder={'søk i katalog'}
+        onChange={(e) => updateInput(e.target.value)}
+      />
+      <div>{displaylist(searchList)}</div>
+    </div>
+  );
+}
