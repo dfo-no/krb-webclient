5fac33450b02781bc5f1e4897f0d1470ad955b1a
Ingrid Medalen <imed@computas.com>
2020-12-21 13:09:23 +0100

    update to develop version

--- src/KravbankKatalogSide/KravbankKatalogSide.tsx
+++ src/KravbankKatalogSide/KravbankKatalogSide.tsx
@@ -1,43 +1,17 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import styles from './KravbankKatalogSide.module.scss';
-<<<<<<< Updated upstream
-import { Link, useHistory } from 'react-router-dom';
-=======
 import { useHistory } from 'react-router-dom';
->>>>>>> Stashed changes
-import data from '../data/katalog1.json';
+import data from '../data/katalog.json';
 import { Katalog } from '../models/Katalog';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { Kravbank } from '../models/Kravbank';
-
-const renderKatalogItems = (katalogItems: Kravbank[]) => {
-  return katalogItems.map((kravbank, index) => (
-    <Draggable key={kravbank.id} draggableId={kravbank.id} index={index}>
-      {(provided) => (
-        <div
-          className={styles.katalogitem}
-          key={kravbank.id}
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <p>{kravbank.tittel}</p>
-          <Link to={'/edit/' + kravbank.id}>
-            <button className={styles.editbutton} type="button">
-              Rediger
-            </button>
-          </Link>
-        </div>
-      )}
-    </Draggable>
-  ));
-};
+import SearchBar from '../SearchBar/SearchBar';
 
 export default function KravbankKatalogSide(this: any): ReactElement {
   const [katalog, setKatalog] = useState<Katalog>({});
+  const [loading, setLoading] = useState<boolean>(true);
   const defaultkatalogitem = [
     {
-      id: '',
+      id: 'default',
       tittel: '',
       beskrivelse: ''
     }
@@ -50,16 +24,21 @@ export default function KravbankKatalogSide(this: any): ReactElement {
   const handleCreateNew = () => (event: any) => {
     history.push(`/kravbank/ny`);
   };
-
-  const handleOnDragEnd = (result: any) => {
-    if (!result.destination) return;
-    const items = katalogitems;
-    console.log(items);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setKatalogItems(items);
+  const fetchKatalog = () => {
+    setKatalog(data as Katalog);
   };
+
+  useEffect(() => {
+    if (Object.values(katalog).length === 0) {
+      fetchKatalog();
+    } else {
+      setLoading(false);
+    }
+  }, [katalog]);
+
+  useEffect(() => {
+    setKatalogItems(Object.values(katalog));
+  }, [katalog]);
 
   return (
     <div className={styles.container}>
@@ -70,22 +49,8 @@ export default function KravbankKatalogSide(this: any): ReactElement {
       >
         Opprett Kravbank
       </button>
-      <div className={styles.katalogcontainer}>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="katalogitems">
-            {(provided) => (
-              <ul
-                className="katalogitems"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {renderKatalogItems(katalogitems)}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
+      <div className={styles.katalogcontainer}></div>
+      {loading ? <div></div> : <SearchBar list={katalogitems} />}
     </div>
   );
 }
