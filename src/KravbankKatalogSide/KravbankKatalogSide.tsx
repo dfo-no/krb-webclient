c59d652dd2de97788e7ca70be45a40ab64688ccf
imed-computas <73219353+imed-computas@users.noreply.github.com>
2020-12-22 11:37:03 +0100

    Krb 7 (#10)
    * new pages with routing based on user actions, form for registration of new kravbank, new models using maps
    
    * fix sonarcloud bug warning
    
    * prettier
    
    * remove unused variables
    
    * remove unused import
    
    * fix merge conflict
    
    * merge
    
    * merge
    
    * update to develop version
    
    * added possibility to add katalog to store
    
    * pr adjustments

--- src/KravbankKatalogSide/KravbankKatalogSide.tsx
+++ src/KravbankKatalogSide/KravbankKatalogSide.tsx
@@ -1,44 +1,21 @@
-import React, { ReactElement, useEffect, useState } from 'react';
-import styles from './KravbankKatalogSide.module.scss';
+import React, { ReactElement } from 'react';
 import { useHistory } from 'react-router-dom';
-import data from '../data/katalog.json';
-import { Katalog } from '../models/Katalog';
+import { connect } from 'react-redux';
+
 import { Kravbank } from '../models/Kravbank';
+import { State } from '../store/index';
 import SearchBar from '../SearchBar/SearchBar';
+import styles from './KravbankKatalogSide.module.scss';
 
-export default function KravbankKatalogSide(this: any): ReactElement {
-  const [katalog, setKatalog] = useState<Katalog>({});
-  const [loading, setLoading] = useState<boolean>(true);
-  const defaultkatalogitem = [
-    {
-      id: 'default',
-      tittel: '',
-      beskrivelse: ''
-    }
-  ];
-  const [katalogitems, setKatalogItems] = useState<Kravbank[]>(
-    defaultkatalogitem
-  );
+interface IProps {
+  kravbanker: Kravbank[];
+}
 
+function KravbankKatalogSide(props: IProps): ReactElement {
   const history = useHistory();
   const handleCreateNew = () => (event: any) => {
     history.push(`/kravbank/ny`);
   };
-  const fetchKatalog = () => {
-    setKatalog(data as Katalog);
-  };
-
-  useEffect(() => {
-    if (Object.values(katalog).length === 0) {
-      fetchKatalog();
-    } else {
-      setLoading(false);
-    }
-  }, [katalog]);
-
-  useEffect(() => {
-    setKatalogItems(Object.values(katalog));
-  }, [katalog]);
 
   return (
     <div className={styles.container}>
@@ -50,7 +27,13 @@ export default function KravbankKatalogSide(this: any): ReactElement {
         Opprett Kravbank
       </button>
       <div className={styles.katalogcontainer}></div>
-      {loading ? <div></div> : <SearchBar list={katalogitems} />}
+      <SearchBar list={props.kravbanker}></SearchBar>
     </div>
   );
 }
+
+const mapStateToProps = (store: State) => {
+  return { kravbanker: store.kravbanker };
+};
+
+export default connect(mapStateToProps)(KravbankKatalogSide);
