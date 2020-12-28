9ffa9d109faed11cf3a6f420edbf0dafb70aefe0
Trond Albinussen <tal@computas.com>
2020-12-28 13:40:31 +0100

    listen on default port 80

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -111,7 +111,7 @@ if (
   process.env.KRB_CONTAINER_ID,
   process.env.KRB_PORT)
 ) {
-  app.listen(process.env.KRB_PORT);
+  app.listen(80);
 } else {
   console.log('Exit: Missing environment variables');
 }
