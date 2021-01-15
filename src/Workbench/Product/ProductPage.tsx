0fdfd9e1e749614c5c0b7fe23090612bb6146142
Ingrid Medalen <imed@computas.com>
2021-01-15 14:34:50 +0100

    routing fix

--- src/Workbench/Product/ProductPage.tsx
+++ src/Workbench/Product/ProductPage.tsx
@@ -1,13 +1,13 @@
-import { ReactElement, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../store/configureStore';
+import { ReactElement } from 'react';
+//import { useDispatch, useSelector } from 'react-redux';
+//import { RootState } from '../../store/configureStore';
 
 export default function ProductPage(): ReactElement {
-  const dispatch = useDispatch();
+  /*const dispatch = useDispatch();
   const { products } = useSelector((state: RootState) => state.kravbank);
   const [productList, setProductList] = useState(products);
   const [showEditor, setShowEdior] = useState(false);
   const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState('');*/
   return <p>Products</p>;
 }
