48e084955a225446a6b9058ba2d7198423ca7668
Trond Albinussen <tal@computas.com>
2021-01-28 10:22:45 +0100

    Rename catalogue to banks

--- src/api/bankApi.ts
+++ src/api/bankApi.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { Bank } from '../models/Bank';
 
 export const fetchAllBanks = async () => {
-  // TODO: rename /catalogue to "banks" or "bank"
-  const response = await axios.get(`http://localhost:3001/catalogue`);
+  const response = await axios.get(`http://localhost:3001/banks`);
   return response.data as Bank[];
 };
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 import jsonRaw from '../db/master.json';
 import { Bank } from './models/Bank';
 
-const bank: Bank[] = jsonRaw.catalogue;
+const bank: Bank[] = jsonRaw.banks;
 
 console.log(bank);
 // for type-checing the master.json file
