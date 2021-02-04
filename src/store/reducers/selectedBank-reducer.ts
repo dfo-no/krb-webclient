fa31ebd7416b43a28c7cc27b62462da85afa463f
imed-computas <73219353+imed-computas@users.noreply.github.com>
2021-02-04 13:04:26 +0100

    Krb 67 (#50)
    * update styling on spec and editor page
    
    * new selectbank reducer + styling of evaluation page

---
+++ src/store/reducers/selectedBank-reducer.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface SelectedBankState {
+  id: number | null;
+}
+
+const initialState: SelectedBankState = { id: null };
+
+const selectedBankSlice = createSlice({
+  name: 'selectedBank',
+  initialState,
+  reducers: {
+    selectBank(state, { payload }: PayloadAction<number>) {
+      state.id = payload;
+    }
+  }
+});
+
+export const { selectBank } = selectedBankSlice.actions;
+
+export default selectedBankSlice.reducer;
