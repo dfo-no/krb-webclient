27e8fa2aed5169d1644e9f41036d402bfddf1376
Ingrid Medalen <imed@computas.com>
2021-02-15 13:51:42 +0100

    pr fix

--- src/models/Publication.ts
+++ src/models/Publication.ts
@@ -3,9 +3,5 @@ export interface Publication {
   comment: string;
   date: string;
   version: number;
-<<<<<<< HEAD
   bankId: string;
-=======
-  bankId: number;
->>>>>>> 1b1561bf90b7b599d9dee4f3a1dd2ebc60852f44
 }
