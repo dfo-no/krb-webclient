12b2283447a3941807607c07bac78d78f2ba1e62
Trond Albinussen <67086809+talcomputas@users.noreply.github.com>
2021-03-25 08:55:49 +0100

    Krb 71, krb-135 (#91)
    * Removed unused eslint jsx-a11y
    
    * Creating a project should set version to 0
    
    * new way to use Publish
    
    * Remove unused code

--- src/Workbench/Product/ProductPage.tsx
+++ src/Workbench/Product/ProductPage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { ReactElement, useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
