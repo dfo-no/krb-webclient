446698aa05ddd7ebd00e226e0ff20d1e587ed8f8
Ingrid Medalen <imed@computas.com>
2021-04-21 15:14:40 +0200

    new text alternative form

---
+++ src/SpecEditor/Requirement/AlternativeForms/TextAlternativeForm.tsx
@@ -0,0 +1,132 @@
+import React, { ReactElement } from 'react';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Joi from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import { IValueAlternative } from '../../../models/IValueAlternative';
+import { RequirementAnswer } from '../../../models/RequirementAnswer';
+import {
+  editAnswer,
+  editProductAnswer
+} from '../../../store/reducers/spesification-reducer';
+import { RootState } from '../../../store/store';
+import { ITextAlternative } from '../../../models/ITextAlternative';
+
+const textSchema = Joi.object().keys({
+  id: Joi.string().required(),
+  type: Joi.string().equal('text').required(),
+  max: Joi.number().required(),
+  text: Joi.string().trim().max(Joi.ref('max')).required()
+});
+
+interface IProps {
+  parentAnswer: RequirementAnswer;
+}
+
+type FormValues = {
+  text: string;
+  max: number;
+};
+
+export default function TextForm({ parentAnswer }: IProps): ReactElement {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: joiResolver(textSchema),
+    defaultValues: {
+      ...(parentAnswer.alternative as ITextAlternative)
+    }
+  });
+  const { productId } = useSelector(
+    (state: RootState) => state.selectedSpecProduct
+  );
+  const item = parentAnswer.alternative as ITextAlternative;
+  const dispatch = useDispatch();
+
+  if (!productId && parentAnswer.type === 'product') {
+    return <p>No product selected</p>;
+  }
+
+  const saveValues = (post: FormValues) => {
+    const newAlt = {
+      ...item
+    };
+    const newAnswer = {
+      ...parentAnswer
+    };
+    newAlt.max = post.max;
+    newAlt.text = post.text;
+    newAnswer.alternative = newAlt;
+
+    if (newAnswer.type === 'requirement')
+      dispatch(editAnswer({ answer: newAnswer }));
+    if (newAnswer.type === 'product' && productId !== null)
+      dispatch(editProductAnswer({ answer: newAnswer, productId }));
+  };
+
+  return (
+    <Card className="mb-3">
+      <Card.Body>
+        <h6>Alternative: Value</h6>
+        <Form onSubmit={handleSubmit(saveValues)}>
+          <Form.Control
+            as="input"
+            type="hidden"
+            name="id"
+            ref={register}
+            isInvalid={!!errors.id}
+          />
+
+          <Form.Control
+            as="input"
+            type="hidden"
+            name="type"
+            ref={register}
+            isInvalid={!!errors.type}
+          />
+          <Form.Group as={Row}>
+            <Form.Label column sm="2">
+              Text
+            </Form.Label>
+            <Col sm="4">
+              <Form.Control
+                type="input"
+                name="text"
+                ref={register}
+                isInvalid={!!errors.text}
+              />
+              {errors.text && (
+                <Form.Control.Feedback type="invalid">
+                  {errors.text.message}
+                </Form.Control.Feedback>
+              )}
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label column sm="2">
+              Maximum
+            </Form.Label>
+            <Col sm="4">
+              <Form.Control
+                type="number"
+                name="max"
+                ref={register}
+                isInvalid={!!errors.max}
+              />
+              {errors.max && (
+                <Form.Control.Feedback type="invalid">
+                  {errors.max.message}
+                </Form.Control.Feedback>
+              )}
+            </Col>
+          </Form.Group>
+
+          <Button type="submit"> Save</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
