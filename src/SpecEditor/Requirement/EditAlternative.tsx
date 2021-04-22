2385dd289dc39d40dd7135978969d2578716294d
Ingrid Medalen <imed@computas.com>
2021-04-22 10:21:42 +0200

    new fileUpload alternative form

--- src/SpecEditor/Requirement/EditAlternative.tsx
+++ src/SpecEditor/Requirement/EditAlternative.tsx
@@ -3,6 +3,7 @@ import React, { ReactElement } from 'react';
 import Container from 'react-bootstrap/Container';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
+import FileInputForm from './AlternativeForms/FileInputForm';
 import ValueForm from './AlternativeForms/IValueAlternativeForm';
 import NoProperties from './AlternativeForms/NoProperties';
 
@@ -26,6 +27,7 @@ export default function EditAlternative(): ReactElement {
     <Container fluid className="mt-4">
       <h4>Edit Alternative</h4>
       {item.alternative.type === 'value' && <ValueForm parentAnswer={item} />}
+      {item.alternative.type === 'fileUpload' && <FileInputForm />}
       {item.alternative.type === 'codelist' && <NoProperties />}
       {item.alternative.type === 'yesNo' && <NoProperties />}
     </Container>
