8f8ba05153e3b5386bbabcc92fadfdda1b9eeed6
imed-computas <73219353+imed-computas@users.noreply.github.com>
2021-04-22 10:29:42 +0200

    Krb 169 (#120)
    * new fileUpload and yes/no alternative
    
    * edit alternative, with support for valueform
    
    * edit Value form, editpage for all types of alternatives
    
    * implement alternative select on product page
    
    * chane defaultvalues
    
    * new component for options that does not have properties to edit
    
    * new text alternative form
    
    * remove unused import
    
    * add textalternative to render function on edit page
    
    * pr ifx

--- src/SpecEditor/Requirement/EditAlternative.tsx
+++ src/SpecEditor/Requirement/EditAlternative.tsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import ValueForm from './AlternativeForms/IValueAlternativeForm';
 import NoProperties from './AlternativeForms/NoProperties';
+import TextAlternativeForm from './AlternativeForms/TextAlternativeForm';
 
 export default function EditAlternative(): ReactElement {
   const { alternativeId } = useSelector(
@@ -26,6 +27,9 @@ export default function EditAlternative(): ReactElement {
     <Container fluid className="mt-4">
       <h4>Edit Alternative</h4>
       {item.alternative.type === 'value' && <ValueForm parentAnswer={item} />}
+      {item.alternative.type === 'text' && (
+        <TextAlternativeForm parentAnswer={item} />
+      )}
       {item.alternative.type === 'codelist' && <NoProperties />}
       {item.alternative.type === 'yesNo' && <NoProperties />}
     </Container>
