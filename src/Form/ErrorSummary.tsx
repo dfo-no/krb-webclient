3fc0db5cad0bfb6023f1175b48b7342b66557c54
Trond Albinussen <tal@computas.com>
2021-05-03 16:44:07 +0200

    Error Summary supports useFieldArray

--- src/Form/ErrorSummary.tsx
+++ src/Form/ErrorSummary.tsx
@@ -1,29 +1,45 @@
-import { ErrorMessage } from '@hookform/error-message';
-import React from 'react';
-import { FieldErrors } from 'react-hook-form';
+import React, { ReactElement } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import { FieldError, FieldErrors } from 'react-hook-form';
 
 type ErrorSummaryProps<T> = {
   errors: FieldErrors<T>;
 };
-export default function ErrorSummary<T>({ errors }: ErrorSummaryProps<T>) {
-  if (Object.keys(errors).length === 0) {
-    console.log('no errors');
+export default function ErrorSummary<T>({
+  errors
+}: ErrorSummaryProps<T>): ReactElement | null {
+  if (process.env.NODE_ENV !== 'development') {
     return null;
   }
+
+  if (Object.keys(errors).length === 0) {
+    return null;
+  }
+
+  const errorMessages = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [key, value] of Object.entries(errors)) {
+    if (Array.isArray(value)) {
+      const arrayFieldError = value as FieldError[];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const [key2, v] of Object.entries(arrayFieldError)) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [key3, v2] of Object.entries(v)) {
+          const g = v2 as FieldError;
+          errorMessages.push(g.message);
+        }
+      }
+    } else {
+      const f = value as FieldError;
+      errorMessages.push(f.message);
+    }
+  }
+
   return (
-    <div>
-      {Object.keys(errors).map((fieldName) => (
-        <ErrorMessage
-          errors={errors}
-          name={fieldName as any}
-          render={({ messages }) =>
-            messages &&
-            Object.entries(messages).map(([type, message]) => (
-              <p key={type}>{message}</p>
-            ))
-          }
-        />
+    <Alert variant="danger">
+      {errorMessages.map((msg) => (
+        <div key={msg}>{msg}</div>
       ))}
-    </div>
+    </Alert>
   );
 }
