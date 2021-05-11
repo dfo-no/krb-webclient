313c9e1484a18f1e5389322e8fb658e8525189fc
imed-computas <imed@computas.com>
2021-05-11 09:36:29 +0200

    azure function converting json to simple pdf, example download func for use

--- src/SpecEditor/Download/DownloadPage.tsx
+++ src/SpecEditor/Download/DownloadPage.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react';
 import fileDownload from 'js-file-download';
+// import axios from 'axios';
+// import { saveAs } from 'file-saver';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
@@ -8,7 +10,28 @@ import { RootState } from '../../store/store';
 export default function SpecPage(): ReactElement {
   const { spec } = useSelector((state: RootState) => state.specification);
 
+  const convertJSONToPdf = () => {
+    /* axios
+      .post(
+        'https://krb-pdf.azurewebsites.net/api/jsonToPdf?code=KldgzqSh6itpBgIrabDhAePvkKX2INVJLERGalimHK3l1IJ1ZZp9ng==',
+        {
+          bankName: spec.title,
+          responseType: 'arrayBuffer'
+        }
+      )
+      .then(function (response) {
+        const blob = new Blob([new Uint8Array(response.data.data)], {
+          type: 'application/pdf'
+        });
+        return blob;
+      })
+      .then(function (blob) {
+        saveAs(blob, 'foo.pdf');
+      }); */
+  };
+
   const onDownLoad = () => {
+    convertJSONToPdf();
     fileDownload(JSON.stringify(spec), `${spec.title}-specfication.json`);
   };
 
