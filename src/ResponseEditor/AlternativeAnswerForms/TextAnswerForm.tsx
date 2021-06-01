ab2714e4f48ca719fb54e589070811c0a3cd19c3
imed-computas <73219353+imed-computas@users.noreply.github.com>
2021-06-01 09:59:55 +0200

    Krb 192 (#145)
    * add answer and defualtvalues for requirementanswer in response
    
    * remove unused var
    
    * textalternative form
    
    * pr fix

---
+++ src/ResponseEditor/AlternativeAnswerForms/TextAnswerForm.tsx
@@ -0,0 +1,106 @@
+import React, { ReactElement } from 'react';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import { useTranslation } from 'react-i18next';
+import Joi from 'joi';
+import { IRequirementAnswer } from '../../models/IRequirementAnswer';
+import { RootState } from '../../store/store';
+import { ITextQuestion } from '../../models/ITextQuestion';
+import ErrorSummary from '../../Form/ErrorSummary';
+import { addRequirementAnswer } from '../../store/reducers/response-reducer';
+import QuestionEnum from '../../models/QuestionEnum';
+
+interface IProps {
+  parentAnswer: IRequirementAnswer;
+}
+
+export const ResponseTextSchema = Joi.object().keys({
+  id: Joi.string().required(),
+  type: Joi.string().equal(QuestionEnum.Q_TEXT).required(),
+  config: Joi.object().keys({
+    max: Joi.number().required().min(0)
+  }),
+  answer: Joi.object().keys({
+    text: Joi.string().disallow(null, '').required()
+  })
+});
+
+export default function ITextAnswer({ parentAnswer }: IProps): ReactElement {
+  const { response } = useSelector((state: RootState) => state.response);
+  const index = response.requirementAnswers.findIndex(
+    (answer) => answer.reqTextId === parentAnswer.reqTextId
+  );
+  const defaultVal =
+    index === -1
+      ? (parentAnswer.alternative as ITextQuestion)
+      : (response.requirementAnswers[index].alternative as ITextQuestion);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<ITextQuestion>({
+    resolver: joiResolver(ResponseTextSchema),
+    defaultValues: {
+      ...defaultVal
+    }
+  });
+  const { productId } = useSelector(
+    (state: RootState) => state.selectedSpecProduct
+  );
+  const item = parentAnswer.alternative as ITextQuestion;
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  if (!productId && parentAnswer.type === 'product') {
+    return <p>No product selected</p>;
+  }
+
+  const saveValues = (post: ITextQuestion) => {
+    const newAnswer = {
+      ...parentAnswer
+    };
+    newAnswer.alternative = post;
+    dispatch(addRequirementAnswer(newAnswer));
+  };
+
+  return (
+    <Card className="mb-3">
+      <Card.Body>
+        <h6>Alternative: Text</h6>
+        <Form onSubmit={handleSubmit(saveValues)}>
+          <Form.Control
+            as="input"
+            type="hidden"
+            {...register('id')}
+            isInvalid={!!errors.id}
+          />
+
+          <Form.Control
+            as="input"
+            type="hidden"
+            {...register('type')}
+            isInvalid={!!errors.type}
+          />
+          <Form.Control
+            as="input"
+            type="hidden"
+            {...register('config.max')}
+            isInvalid={!!errors.config?.max}
+          />
+          <Form.Control
+            as="input"
+            {...register('answer.text')}
+            maxLength={item.config.max}
+          />
+
+          <Button type="submit">{t('save')}</Button>
+          <ErrorSummary errors={errors} />
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/ResponseEditor/Requirement/RequirementPage.tsx
+++ src/ResponseEditor/Requirement/RequirementPage.tsx
@@ -13,6 +13,8 @@ import { RootState } from '../../store/store';
 import styles from './RequirementView.module.scss';
 import ISliderAnswer from '../AlternativeAnswerForms/ISliderAnswer';
 import { IRequirementAnswer } from '../../models/IRequirementAnswer';
+import ITextAnswer from '../AlternativeAnswerForms/TextAnswerForm';
+import QuestionEnum from '../../models/QuestionEnum';
 
 export default function RequirementPage(): ReactElement {
   const { response } = useSelector((state: RootState) => state.response);
@@ -78,8 +80,14 @@ export default function RequirementPage(): ReactElement {
         return (
           <Card key={req.id} className="ml-3 mb-3">
             <Card.Body>{requirementText}</Card.Body>
-            {selectedAnswer.alternative.type === 'Q_SLIDER' && (
+            {selectedAnswer.alternative.type === QuestionEnum.Q_SLIDER && (
               <ISliderAnswer
+                key={selectedAnswer.id}
+                parentAnswer={selectedAnswer}
+              />
+            )}
+            {selectedAnswer.alternative.type === QuestionEnum.Q_TEXT && (
+              <ITextAnswer
                 key={selectedAnswer.id}
                 parentAnswer={selectedAnswer}
               />
