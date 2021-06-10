d714d787519b869e5f7a17f27f6a44e99adc1855
Trond Albinussen <tal@computas.com>
2021-06-10 11:04:16 +0200

    Download pdf file w/attchment instead of raw json

--- src/SpecEditor/Download/DownloadPage.tsx
+++ src/SpecEditor/Download/DownloadPage.tsx
@@ -1,17 +1,39 @@
 import React, { ReactElement } from 'react';
-import fileDownload from 'js-file-download';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { AxiosResponse } from 'axios';
 import { RootState } from '../../store/store';
+import { httpPost } from '../../api/http';
+import { Specification } from '../../models/Specification';
 
 export default function SpecPage(): ReactElement {
   const { t } = useTranslation();
   const { spec } = useSelector((state: RootState) => state.specification);
 
   const onDownLoad = () => {
-    fileDownload(JSON.stringify(spec), `${spec.title}-specfication.json`);
+    httpPost<Specification, AxiosResponse<File>>(
+      `${process.env.REACT_APP_JAVA_API_URL}/generatePdf`,
+      spec,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/pdf'
+        },
+        responseType: 'blob'
+      }
+    ).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'response.pdf');
+      document.body.appendChild(link);
+      link.click();
+      setTimeout(() => {
+        window.URL.revokeObjectURL(link.toString());
+      }, 200);
+    });
   };
 
   return (
