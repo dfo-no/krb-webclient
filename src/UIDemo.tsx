e0a89a03a11407be760f76c89e6137897809cc49
Trond Albinussen <tal@computas.com>
2021-06-21 15:13:35 +0200

    useFieldArray example

--- src/UIDemo.tsx
+++ src/UIDemo.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-console */
 import DateFnsUtils from '@date-io/date-fns';
 import { joiResolver } from '@hookform/resolvers/joi';
 import {
@@ -17,64 +19,325 @@ import {
   MuiPickersUtilsProvider
 } from '@material-ui/pickers';
 import Joi from 'joi';
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { get, has } from 'lodash';
+import React, { ReactElement, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
-import { Controller, useForm } from 'react-hook-form';
+import {
+  Control,
+  Controller,
+  FormState,
+  useFieldArray,
+  useForm,
+  UseFormRegister
+} from 'react-hook-form';
+import ErrorSummary from './Form/ErrorSummary';
 
 let renderCount = 0;
 
-const defaultValues = {
+const defaultValues: ICart = {
   cart: {
-    fromDate: '2021-06-24T08:45:00.000Z',
-    from: 'OSL',
-    firstName: 'Trond',
-    meal: true,
-    gender: 'male',
-    leisure: true,
-    fun: 91
+    fromDate: '2021-06-28T08:45:00.000Z',
+    from: 'BGO',
+    meal: false,
+    firstName: 'Maria',
+    gender: 'female',
+    leisure: false,
+    fun: 9,
+    email: 'maria@computas.com',
+    variants: [
+      {
+        fromDate: '2021-03-24T08:45:00.000Z',
+        from: 'BGO',
+        meal: false,
+        firstName: 'Maria',
+        gender: 'female',
+        leisure: false,
+        fun: 9,
+        email: 'maria@computas.com'
+      }
+    ]
   }
 };
 
-type Form = {
-  cart: {
-    fromDate: string;
-    from: string;
-    firstName: string;
-    meal: boolean;
-    gender: string;
-    leisure: boolean;
-    fun: number;
-  };
-};
+interface IFirst {
+  meal: boolean;
+  fromDate: string;
+  from: string;
+  gender: string;
+  firstName: string;
+
+  leisure: boolean;
+  fun: number;
+  email: string;
+  variants: ISecond[];
+}
+
+interface ISecond {
+  meal: boolean;
+  fromDate: string;
+  from: string;
+  firstName: string;
+  gender: string;
+  leisure: boolean;
+  fun: number;
+  email: string;
+}
+
+interface ICart {
+  cart: IFirst;
+}
+
+const variantSchema = Joi.object().keys({
+  meal: Joi.boolean().equal(true).required(),
+  fromDate: Joi.date().iso().less('06-25-2021').required(),
+  from: Joi.string().equal('OSL').required(),
+  firstName: Joi.string().equal('Trond').max(15).required(),
+  gender: Joi.string().valid('male').required(),
+  leisure: Joi.boolean().valid(true).required(),
+  fun: Joi.number().min(90).max(100).required(),
+  email: Joi.string()
+    .email({ tlds: false })
+    .equal('tal@computas.com')
+    .required()
+});
 
 const formSchema = Joi.object().keys({
   cart: Joi.object().keys({
-    firstName: Joi.string().equal('Trond').max(15).required(),
     meal: Joi.boolean().equal(true).required(),
-    gender: Joi.string().valid('male').required(),
+    fromDate: Joi.date().iso().less('06-25-2021').required(),
     from: Joi.string().equal('OSL').required(),
+    firstName: Joi.string().equal('Trond').max(15).required(),
+    gender: Joi.string().valid('male').required(),
     leisure: Joi.boolean().valid(true).required(),
     fun: Joi.number().min(90).max(100).required(),
-    fromDate: Joi.date().iso().less('06-25-2021').required()
+    email: Joi.string()
+      .email({ tlds: false })
+      .equal('tal@computas.com')
+      .required(),
+    variants: Joi.array().items(variantSchema)
   })
 });
 
+interface IProps {
+  control: Control<ICart>;
+  register: UseFormRegister<ICart>;
+  formState: FormState<ICart>;
+  getValues: any;
+}
+
+function Variants({
+  control,
+  register,
+  formState,
+  getValues
+}: IProps): ReactElement {
+  const { errors } = formState;
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: `cart.variants` as 'cart.variants'
+  });
+
+  return (
+    <Container>
+      <h3>UseFieldArray example</h3>
+
+      {fields.map((item, index) => {
+        return (
+          <div key={index}>
+            <section id="input-checkbox">
+              <span>MUI Checkbox (meal)</span>
+
+              <Controller
+                name={`cart.variants.${index}.meal` as const}
+                control={control}
+                defaultValue={getValues(`cart.variants[${index}].meal`)}
+                render={({ field: p }) => (
+                  <Checkbox
+                    inputRef={p.ref}
+                    {...p}
+                    checked={!!p.value}
+                    onChange={(e) => p.onChange(e.target.checked)}
+                  />
+                )}
+              />
+              {has(errors, `cart.variants[${index}].meal`) && (
+                <p style={{ color: 'red' }}>
+                  {get(errors, `cart.variants[${index}].meal.message`)}
+                </p>
+              )}
+            </section>
+            <section>
+              <span>MUI Picker (fromDate)</span>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Controller
+                  name={`cart.variants.${index}.fromDate` as const}
+                  control={control}
+                  defaultValue={getValues(`cart.variants[${index}].fromDate`)}
+                  render={({ field: { ref, ...rest } }) => (
+                    <KeyboardDatePicker
+                      margin="normal"
+                      id="date-picker-dialog"
+                      format="dd/MM/yyyy"
+                      KeyboardButtonProps={{
+                        'aria-label': 'change date'
+                      }}
+                      {...rest}
+                    />
+                  )}
+                />
+              </MuiPickersUtilsProvider>
+              {has(errors, `cart.variants[${index}].fromDate`) && (
+                <p style={{ color: 'red' }}>
+                  {get(errors, `cart.variants[${index}].fromDate.message`)}
+                </p>
+              )}
+            </section>
+            <section id="input-select">
+              <span>MUI Select (from)</span>
+              <Controller
+                name={`cart.variants.${index}.from` as const}
+                control={control}
+                defaultValue={getValues(`cart.variants[${index}].from`)}
+                render={({ field }) => (
+                  <Select {...field}>
+                    <MenuItem value="OSL">Oslo Gardermoen</MenuItem>
+                    <MenuItem value="TRD">Trondheim Værnes</MenuItem>
+                    <MenuItem value="BGO">Bergen Flesland</MenuItem>
+                  </Select>
+                )}
+              />
+              {has(errors, `cart.variants[${index}].from`) && (
+                <p style={{ color: 'red' }}>
+                  {get(errors, `cart.variants[${index}].from.message`)}
+                </p>
+              )}
+            </section>
+            <section id="input-radio-group">
+              <span>MUI Radio Group</span>
+              <Controller
+                name={`cart.variants.${index}.gender` as const}
+                control={control}
+                defaultValue={getValues(`cart.variants.${index}.gender`)}
+                render={({ field }) => (
+                  <RadioGroup aria-label="gender" {...field} name="gender1">
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio />}
+                      label="Female"
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio />}
+                      label="Male"
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={<Radio />}
+                      label="Other"
+                    />
+                  </RadioGroup>
+                )}
+              />
+              {has(errors, `cart.variants[${index}].gender`) && (
+                <p style={{ color: 'red' }}>
+                  {get(errors, `cart.variants[${index}].gender.message`)}
+                </p>
+              )}
+            </section>
+            <section id="input-textField">
+              <span>MUI TextField (firstName)</span>
+              <Controller
+                name={`cart.variants.${index}.firstName` as const}
+                defaultValue={getValues(`cart.variants.${index}.firstName`)}
+                render={({ field }) => <TextField {...field} />}
+                control={control}
+              />
+              {has(errors, `cart.variants[${index}].firstName`) && (
+                <p style={{ color: 'red' }}>
+                  {get(errors, `cart.variants[${index}].firstName.message`)}
+                </p>
+              )}
+            </section>
+
+            <section id="input-switch">
+              <span>Leisure</span>
+              <Controller
+                name={`cart.variants.${index}.leisure` as const}
+                control={control}
+                defaultValue={getValues(`cart.variants.${index}.leisure`)}
+                render={({ field }) => (
+                  <Switch
+                    {...field}
+                    onChange={(e) => field.onChange(e.target.checked)}
+                    checked={field.value}
+                  />
+                )}
+              />
+              {has(errors, `cart.variants[${index}].leisure`) && (
+                <p style={{ color: 'red' }}>
+                  {get(errors, `cart.variants[${index}].leisure.message`)}
+                </p>
+              )}
+            </section>
+            <section>
+              <span>Fun</span>
+              <Controller
+                name={`cart.variants.${index}.fun` as const}
+                control={control}
+                defaultValue={getValues(`cart.variants.${index}.fun`)}
+                render={({ field }) => (
+                  <Slider
+                    {...field}
+                    onChange={(_, value) => {
+                      field.onChange(value);
+                    }}
+                    valueLabelDisplay="auto"
+                    step={1}
+                  />
+                )}
+              />
+              {has(errors, `cart.variants[${index}].fun`) && (
+                <p style={{ color: 'red' }}>
+                  {get(errors, `cart.variants[${index}].fun.message`)}
+                </p>
+              )}
+            </section>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                {...register(`cart.variants.${index}.email` as const)}
+                placeholder="Enter email"
+                defaultValue={getValues(`cart.variants.${index}.email`)}
+                isInvalid={!!has(errors, `cart.variants[${index}].email`)}
+              />
+              {has(errors, `cart.variants[${index}].email`) && (
+                <Form.Control.Feedback type="invalid">
+                  {get(errors, `cart.variants[${index}].email.message`)}
+                </Form.Control.Feedback>
+              )}
+            </Form.Group>
+          </div>
+        );
+      })}
+    </Container>
+  );
+}
+
 export default function UIDemo(): JSX.Element {
-  const methods = useForm<Form>({
+  const methods = useForm<ICart>({
     defaultValues,
     resolver: joiResolver(formSchema)
   });
-  const {
-    handleSubmit,
-    formState: { errors },
-    reset,
-    control
-  } = methods;
+  const { handleSubmit, formState, reset, control, register, getValues } =
+    methods;
 
   const [result, setResult] = useState('');
   renderCount += 1;
+
+  const { errors } = formState;
 
   return (
     <Container>
@@ -83,7 +346,8 @@ export default function UIDemo(): JSX.Element {
         <Badge variant="info">{renderCount}</Badge>
       </h3>
       <form
-        onSubmit={handleSubmit((data: Form) => {
+        onSubmit={handleSubmit((data: ICart) => {
+          console.log(data);
           setResult(JSON.stringify(data, null, 2));
         })}
       >
@@ -102,12 +366,54 @@ export default function UIDemo(): JSX.Element {
                 />
               )}
             />
+            {errors?.cart?.meal && (
+              <p style={{ color: 'red' }}>{errors.cart.meal.message}</p>
+            )}
           </section>
 
-          {errors?.cart?.meal && <p>{errors.cart.meal.message}</p>}
+          <section>
+            <span>MUI Picker (fromDate)</span>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Controller
+                name={`cart.fromDate` as const}
+                control={control}
+                render={({ field: { ref, ...rest } }) => (
+                  <KeyboardDatePicker
+                    margin="normal"
+                    id="date-picker-dialog"
+                    format="dd/MM/yyyy"
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date'
+                    }}
+                    {...rest}
+                  />
+                )}
+              />
+            </MuiPickersUtilsProvider>
+            {errors?.cart?.fromDate && (
+              <p style={{ color: 'red' }}>{errors.cart.fromDate.message}</p>
+            )}
+          </section>
+          <section id="input-select">
+            <span>MUI Select (from)</span>
+            <Controller
+              name={`cart.from` as const}
+              control={control}
+              render={({ field }) => (
+                <Select {...field}>
+                  <MenuItem value="OSL">Oslo Gardermoen</MenuItem>
+                  <MenuItem value="TRD">Trondheim Værnes</MenuItem>
+                  <MenuItem value="BGO">Bergen Flesland</MenuItem>
+                </Select>
+              )}
+            />
+            {errors?.cart?.from && (
+              <p style={{ color: 'red' }}>{errors.cart.from.message}</p>
+            )}
+          </section>
 
           <section id="input-radio-group">
-            <span>MUI Radio Group</span>
+            <span>MUI Radio Group (gender)</span>
             <Controller
               rules={{ required: true }}
               name={`cart.gender` as const}
@@ -132,37 +438,22 @@ export default function UIDemo(): JSX.Element {
                 </RadioGroup>
               )}
             />
+            {errors?.cart?.gender && (
+              <p style={{ color: 'red' }}>{errors.cart.gender.message}</p>
+            )}
           </section>
 
-          {errors?.cart?.gender && <p>{errors.cart.gender.message}</p>}
-
           <section id="input-textField">
-            <span>MUI TextField</span>
+            <span>MUI TextField (firstName)</span>
             <Controller
               name={`cart.firstName` as const}
               render={({ field }) => <TextField {...field} />}
               control={control}
             />
+            {errors.cart?.firstName && (
+              <p style={{ color: 'red' }}>{errors.cart.firstName.message}</p>
+            )}
           </section>
-
-          {errors.cart?.firstName && <p>{errors.cart.firstName.message}</p>}
-
-          <section id="input-select">
-            <span>MUI Select</span>
-            <Controller
-              name={`cart.from` as const}
-              control={control}
-              render={({ field }) => (
-                <Select {...field}>
-                  <MenuItem value="OSL">Oslo Gardermoen</MenuItem>
-                  <MenuItem value="TRD">Trondheim Værnes</MenuItem>
-                  <MenuItem value="BGO">Bergen Flesland</MenuItem>
-                </Select>
-              )}
-            />
-          </section>
-
-          {errors?.cart?.from && <p>{errors.cart.from.message}</p>}
 
           <section id="input-switch">
             <span>Leisure</span>
@@ -177,53 +468,51 @@ export default function UIDemo(): JSX.Element {
                 />
               )}
             />
-          </section>
-
-          {errors?.cart?.leisure && <p>{errors.cart.leisure.message}</p>}
-        </div>
-
-        <section>
-          <span>Fun</span>
-          <Controller
-            name={`cart.fun` as const}
-            control={control}
-            render={({ field }) => (
-              <Slider
-                {...field}
-                onChange={(_, value) => {
-                  field.onChange(value);
-                }}
-                valueLabelDisplay="auto"
-                step={1}
-              />
+            {errors?.cart?.leisure && (
+              <p style={{ color: 'red' }}>{errors.cart.leisure.message}</p>
             )}
-          />
-          {errors?.cart?.fun && <p>{errors.cart.fun.message}</p>}
-        </section>
-
-        <section>
-          <span>MUI Picker</span>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          </section>
+          <section>
+            <span>Fun</span>
             <Controller
-              name={`cart.fromDate` as const}
+              name={`cart.fun` as const}
               control={control}
-              render={({ field: { ref, ...rest } }) => (
-                <KeyboardDatePicker
-                  margin="normal"
-                  id="date-picker-dialog"
-                  format="dd/MM/yyyy"
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date'
+              render={({ field }) => (
+                <Slider
+                  {...field}
+                  onChange={(_, value) => {
+                    field.onChange(value);
                   }}
-                  {...rest}
+                  valueLabelDisplay="auto"
+                  step={1}
                 />
               )}
             />
-          </MuiPickersUtilsProvider>
-          {errors?.cart?.fromDate && <p>{errors.cart.fromDate.message}</p>}
-        </section>
-
-        <br />
+            {errors?.cart?.fun && (
+              <p style={{ color: 'red' }}>{errors.cart.fun.message}</p>
+            )}
+          </section>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              {...register(`cart.email` as const)}
+              placeholder="Enter email"
+              isInvalid={!!errors.cart?.email}
+            />
+            {errors.cart?.email && (
+              <Form.Control.Feedback type="invalid">
+                {errors.cart.email.message}
+              </Form.Control.Feedback>
+            )}
+          </Form.Group>
+        </div>
+        <Variants
+          control={control}
+          register={register}
+          formState={formState}
+          getValues={getValues}
+        />
 
         <Button
           type="button"
@@ -237,6 +526,7 @@ export default function UIDemo(): JSX.Element {
         <Button type="submit" id="submit">
           submit
         </Button>
+        <ErrorSummary errors={errors} />
       </form>
       <Paper elevation={1}>
         <pre className="code">{result}</pre>
