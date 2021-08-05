6eb3724c3093204c226c0bee353437e00633f435
Trond Albinussen <tal@computas.com>
2021-08-05 11:00:06 +0200

    removed ajv, it was a stupid idea to test this

--- src/Master.test.tsx
+++ src/Master.test.tsx
@@ -1,13 +1,7 @@
-import Ajv from 'ajv';
-import schema from '../db/schema.json';
-import data from './testData.json';
+import React from 'react';
 
-test('master.json is valid according to schema', () => {
-  // TODO: remove strict = false when models are more mature
-  const ajv = new Ajv({ strict: false });
-  const validate = ajv.compile(schema);
-  expect(validate(data)).toBeTruthy();
-  expect(validate.errors).toBeFalsy();
+test('dummy test', () => {
+  expect(true).toBeTruthy();
 });
 
 // for type-checing the master.json file
