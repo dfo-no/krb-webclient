913bd53d9aff961a47270ef1087d20fdcb613ed4
Trond Albinussen <67086809+talcomputas@users.noreply.github.com>
2021-09-14 10:04:07 +0200

    New Program Nexus (#192)

---
+++ src/common/Nexus.test.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Bank } from '../models/Bank';
+import ModelType from '../models/ModelType';
+import Nexus from './Nexus';
+
+test('Nexus', () => {
+  const id = uuidv4();
+  const bank: Bank = {
+    id,
+    title: 'En test bank',
+    type: ModelType.bank,
+    description: '',
+    needs: [],
+    codelist: [],
+    products: [],
+    version: 0,
+    publications: []
+  };
+
+  const app = Nexus.getInstance();
+
+  const result = app.load(bank).printTitle().getTitle();
+
+  expect(result).toEqual('En test bank');
+});
