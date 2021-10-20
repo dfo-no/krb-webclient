1fdddfb5922e17a46edebbe29cd51814c11ee1e5
Trond Albinussen <tal@computas.com>
2021-10-20 14:36:04 +0200

    Search polytree for paths + tests

--- src/common/Tree.test.ts
+++ src/common/Tree.test.ts
@@ -1,28 +1,249 @@
-import { BaseModel2, createTree, Parentable2, searchTree } from './Tree';
+import { get } from 'lodash';
+import ModelType from '../models/ModelType';
+import { Parentable } from '../models/Parentable';
+import { Tag } from '../models/Tag';
+import { createPolyTree, createTree, getPaths } from './Tree';
 
 describe('Tree', () => {
-  it('Should be nodeable', () => {
-    interface Foo extends BaseModel2 {
-      name: string;
-    }
-
-    const data: Parentable2<Foo>[] = [
-      { id: '56', parent: '62', name: '56' },
-      { id: '81', parent: '80', name: '81' },
-      { id: '74', parent: null, name: '74' },
-      { id: '76', parent: '80', name: '76' },
-      { id: '80', parent: '62', name: '80' },
-      { id: '80', parent: '86', name: '80' },
-      { id: '87', parent: '86', name: '87' },
-      { id: '62', parent: '74', name: '62' },
-      { id: '86', parent: '74', name: '86' }
+  it('createTree', () => {
+    const data: Parentable<Tag>[] = [
+      {
+        id: '76',
+        parent: '80',
+        title: '76',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '62',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '86',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '87',
+        parent: '86',
+        title: '87',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '62',
+        parent: '74',
+        title: '62',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '86',
+        parent: '74',
+        title: '86',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '56',
+        parent: '62',
+        title: '56',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '81',
+        parent: '80',
+        title: '81',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '74',
+        parent: '',
+        title: '74',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      }
     ];
 
     const tree = createTree(data);
-    console.log(JSON.stringify(tree, null, 2));
 
-    const result = searchTree('1', tree);
-    console.log(result);
-    expect(true).toBeTruthy();
+    expect(get(tree, `id`)).toBe('74');
+    expect(get(tree, `title`)).toBe('74');
+
+    expect(get(tree, `children[0].id`)).toBe('62');
+    expect(get(tree, `children[0].parent`)).toBe('74');
+
+    expect(get(tree, `children[1].children[0].id`)).toBe('80');
+    expect(get(tree, `children[1].children[0].parent`)).toBe('86');
+  });
+
+  it('createPolyTree', () => {
+    const data: Parentable<Tag>[] = [
+      {
+        id: '74',
+        parent: '',
+        title: '74',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '62',
+        parent: '74',
+        title: '62',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '62',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '62',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '1',
+        parent: '',
+        title: '1',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '2',
+        parent: '1',
+        title: '2',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '2',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      }
+    ];
+
+    const tree = createPolyTree(data);
+
+    expect(get(tree, `[0].id`)).toBe('74');
+    expect(get(tree, `[1].id`)).toBe('1');
+
+    expect(get(tree, `[0].children[0].id`)).toBe('62');
+    expect(get(tree, `[0].children[0].parent`)).toBe('74');
+
+    expect(get(tree, `[1].children[0].children[0].id`)).toBe('80');
+    expect(get(tree, `[1].children[0].children[0].parent`)).toBe('2');
+  });
+
+  it('searchTree', () => {
+    const data: Parentable<Tag>[] = [
+      {
+        id: '74',
+        parent: '',
+        title: '74',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '62',
+        parent: '74',
+        title: '62',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '81',
+        parent: '62',
+        title: '81',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '62',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '62',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '1',
+        parent: '',
+        title: '1',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '2',
+        parent: '1',
+        title: '2',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      },
+      {
+        id: '80',
+        parent: '2',
+        title: '80',
+        type: ModelType.tag,
+        sourceRel: '',
+        sourceOriginal: ''
+      }
+    ];
+
+    // test multiple paths and found
+    const paths1 = getPaths('80', data);
+    expect(get(paths1, `[0]`)).toEqual(['74', '62', '80']);
+    expect(get(paths1, `[1]`)).toEqual(['1', '2', '80']);
+
+    // test id that does not exist
+    const paths2 = getPaths('800', data);
+    expect(paths2).toEqual([]);
+
+    // test data that does is empty
+    const paths3 = getPaths('800', []);
+    expect(paths3).toEqual([]);
   });
 });
--- src/common/Tree.ts
+++ src/common/Tree.ts
@@ -1,76 +1,155 @@
-export type Nestable2<T extends BaseModel2> = T & {
-  parent: string;
-  children: Nestable2<T>[];
-};
-
-export type Parentable2<T extends BaseModel2> = T & {
-  parent: string | null;
-};
-
-export interface BaseModel2 {
-  id: string;
-}
+import { BaseModel } from '../models/BaseModel';
+import ModelType from '../models/ModelType';
+import { Nestable } from '../models/Nestable';
+import { Parentable } from '../models/Parentable';
 
 interface Acc {
   [key: string]: number;
 }
 
-export function createTree<T extends BaseModel2>(
-  data: Parentable2<T>[]
-): Nestable2<T> {
+/**
+ *  A tree will only contain a single root, and be in object form
+ */
+export function createTree<T extends BaseModel>(
+  data: Parentable<T>[]
+): Nestable<T> {
   const idMapping = data.reduce((acc: Acc, el, i) => {
     // eslint-disable-next-line no-param-reassign
     acc[el.id] = i;
     return acc;
   }, {});
 
-  let root: Nestable2<BaseModel2> = {
+  let root: Nestable<BaseModel> = {
     id: 'gurba',
     parent: 'A',
-    children: []
+    children: [],
+    type: ModelType.tag,
+    sourceOriginal: '',
+    sourceRel: ''
   };
 
   data.forEach((el) => {
-    const element = el as Nestable2<T>;
+    const element = el as Nestable<T>;
     // Handle the root element
-    if (element.parent === null) {
+    if (element.parent === '') {
       root = element;
 
       return;
     }
     // Use our mapping to locate the parent element in our data array
-    const parentEl = data[idMapping[element.parent]] as Nestable2<T>;
+    const parentEl = data[idMapping[element.parent]] as Nestable<T>;
     // Add our current el to its parent's `children` array
     parentEl.children = [
       ...(parentEl.children || []),
       el
-    ] as unknown as Nestable2<T>[];
+    ] as unknown as Nestable<T>[];
 
     if (!element.children) {
       element.children = [];
     }
   });
-  return root as Nestable2<T>;
+  return root as Nestable<T>;
 }
 
-export function searchTree<T extends BaseModel2>(
+/**
+ * A PolyTree can contain more than one root, and is always in Array form
+ */
+export function createPolyTree<T extends BaseModel>(
+  data: Parentable<T>[]
+): Nestable<T>[] {
+  const idMapping = data.reduce((acc: Acc, el, i) => {
+    // eslint-disable-next-line no-param-reassign
+    acc[el.id] = i;
+    return acc;
+  }, {});
+
+  const root: Nestable<BaseModel>[] = [];
+
+  data.forEach((el) => {
+    const element = el as Nestable<T>;
+    // Handle the root element
+    if (element.parent === '') {
+      root.push(element);
+
+      return;
+    }
+    // Use our mapping to locate the parent element in our data array
+    const parentEl = data[idMapping[element.parent]] as Nestable<T>;
+    // Add our current el to its parent's `children` array
+    parentEl.children = [
+      ...(parentEl.children || []),
+      el
+    ] as unknown as Nestable<T>[];
+
+    if (!element.children) {
+      element.children = [];
+    }
+  });
+  return root as Nestable<T>[];
+}
+
+const makePaths = <T extends BaseModel>(
+  tags: Nestable<T>[],
+  res: { [key: string]: [] } = {},
+  prefix: string[] = []
+) => {
+  tags.forEach((tag) => {
+    const values = [...prefix, tag.id];
+
+    if (res[tag.id]) {
+      const existing = res[tag.id];
+      Object.assign(res, { [tag.id]: [existing, values] });
+    } else {
+      Object.assign(res, { [tag.id]: values });
+    }
+
+    if (tag.children) {
+      makePaths(tag.children, res, values);
+    }
+  });
+  return res;
+};
+
+export const getPathsArray = <T extends BaseModel>(
   id: string,
-  tree2: Nestable2<T>
-): Nestable2<T>[] | Nestable2<T>[][] {
-  const loop = (
-    path: Array<typeof node>,
-    node: Nestable2<T>
-  ): Nestable2<T>[] | Nestable2<T>[][] => {
-    return node.id === id
-      ? [node]
-      : node.children.reduce((acc: Nestable2<T>[][], child: Nestable2<T>) => {
-          return acc.concat(loop([...path, node], child));
-        }, []);
-  };
+  data: Parentable<T>[]
+): string[][] | string[] => {
+  const tree = createPolyTree(data);
+  const paths = makePaths(tree);
 
-  return loop([], tree2);
-}
+  return paths[id] ? paths[id] : [];
+};
 
-// export function searchPolyTree<T extends BaseModel2(id: string, tree3, Nestable2<T>) {
+export const normalize2DArray = (data: string[] | string[][]): string[][] => {
+  const result: string[][] = [];
+  data.forEach((element) => {
+    if (Array.isArray(element[0])) {
+      const temp = element as string[];
+      temp.forEach((elem2) => {
+        const tmp1 = elem2 as unknown as string[];
+        result.push(tmp1);
+      });
+    } else {
+      const temp2 = element as string[];
+      result.push(temp2);
+    }
+  });
+  return result;
+};
 
-//}
+export const filterDuplicateArray = (data: string[][]): string[][] => {
+  const stringArrays = data.map((e) => JSON.stringify(e));
+  const uniqueStringArray = new Set(stringArrays);
+  const uniqueArray = Array.from(uniqueStringArray, (x) => JSON.parse(x));
+  return uniqueArray;
+};
+
+export const getPaths = <T extends BaseModel>(
+  id: string,
+  data: Parentable<T>[]
+): string[][] => {
+  const paths = getPathsArray(id, data);
+  const normalizedArray = normalize2DArray(paths);
+  const uniqueArray = filterDuplicateArray(normalizedArray);
+  return uniqueArray;
+};
