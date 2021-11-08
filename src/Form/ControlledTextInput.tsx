0bffaa4c6190a1bd42c28171ca76df82f0fdd9d5
Trond Albinussen <tal@computas.com>
2021-11-08 13:48:06 +0100

    Label for ControlledTextInput

--- src/Form/ControlledTextInput.tsx
+++ src/Form/ControlledTextInput.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
+import Row from 'react-bootstrap/Row';
 import {
   Controller,
   FieldError,
@@ -10,27 +12,55 @@ import {
 
 interface Props<T> extends UseControllerProps<T> {
   error: FieldError | undefined;
+  label?: string;
 }
 
 const ControlledTextInput = <T extends FieldValues>({
   name,
   control,
-  error
+  error,
+  label
 }: Props<T>): React.ReactElement => {
+  if (!label) {
+    return (
+      <Form.Group controlId={name}>
+        <Controller
+          control={control}
+          name={name}
+          render={({ field }) => (
+            <Form.Control type="text" {...field} isInvalid={!!error} />
+          )}
+        />
+        <FormControl.Feedback type="invalid">
+          {error?.message}
+        </FormControl.Feedback>
+      </Form.Group>
+    );
+  }
+
   return (
-    <Form.Group controlId={name}>
-      <Controller
-        control={control}
-        name={name}
-        render={({ field }) => (
-          <Form.Control type="text" {...field} isInvalid={!!error} />
-        )}
-      />
-      <FormControl.Feedback type="invalid">
-        {error?.message}
-      </FormControl.Feedback>
+    <Form.Group controlId={name} as={Row}>
+      <Form.Label column sm={2}>
+        {label}
+      </Form.Label>
+      <Col sm={10}>
+        <Controller
+          control={control}
+          name={name}
+          render={({ field }) => (
+            <Form.Control type="text" {...field} isInvalid={!!error} />
+          )}
+        />
+        <FormControl.Feedback type="invalid">
+          {error?.message}
+        </FormControl.Feedback>
+      </Col>
     </Form.Group>
   );
 };
 
 export default ControlledTextInput;
+
+ControlledTextInput.defaultProps = {
+  label: ''
+};
--- src/Workbench/Project/EditProjectForm.tsx
+++ src/Workbench/Project/EditProjectForm.tsx
@@ -1,13 +1,14 @@
 import { joiResolver } from '@hookform/resolvers/joi';
-import React, { useEffect } from 'react';
+import { get } from 'lodash';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-import { useForm } from 'react-hook-form';
+import { FieldError, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { v4 as uuidv4 } from 'uuid';
+import ControlledTextInput from '../../Form/ControlledTextInput';
 import ErrorSummary from '../../Form/ErrorSummary';
-import InputRow from '../../Form/InputRow';
 import { Alert } from '../../models/Alert';
 import { Bank } from '../../models/Bank';
 import { EditProjectSchema } from '../../models/Project';
@@ -37,48 +38,43 @@ export default function EditProjectForm({
     defaultValues: project
   });
 
-  // Spread object so RHF can register all properties
-  useEffect(() => {
-    if (project) {
-      reset(JSON.parse(JSON.stringify(project)));
-    }
-  }, [project, reset]);
-
-  const onEditProjectSubmit = (post: Bank) => {
-    const alert: Alert = {
-      id: uuidv4(),
-      style: 'success',
-      text: 'Successfully updated projectt'
-    };
+  const onSubmit = (post: Bank) => {
     dispatch(putProjectThunk(post)).then(() => {
+      const alert: Alert = {
+        id: uuidv4(),
+        style: 'success',
+        text: 'Successfully updated projectt'
+      };
       dispatch(addAlert({ alert }));
       toggleShow(false);
     });
   };
 
+  const onReset = () => {
+    reset();
+    toggleShow(false);
+  };
+
   return (
     <Card className="mb-4">
       <Card.Body>
-        <Form onSubmit={handleSubmit(onEditProjectSubmit)}>
-          <InputRow
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <ControlledTextInput
             control={control}
             name="title"
-            errors={errors}
+            error={get(errors, `title`) as FieldError}
             label={t('Title')}
           />
-          <InputRow
+          <ControlledTextInput
             control={control}
             name="description"
-            errors={errors}
+            error={get(errors, `description`) as FieldError}
             label={t('Description')}
           />
           <Button className="mt-2  ml-3" type="submit">
             {t('save')}
           </Button>
-          <Button
-            className="mt-2 ml-3 btn-warning"
-            onClick={() => toggleShow(false)}
-          >
+          <Button className="mt-2 ml-3 btn-warning" onClick={() => onReset()}>
             {t('cancel')}
           </Button>
           <ErrorSummary errors={errors} />
