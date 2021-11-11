2c5b0ea3ea271e821c239684dedd52cdbcf24818
imed-computas <imed@computas.com>
2021-11-11 14:01:54 +0100

    create uploaded prefilledreponse reducer

---
+++ src/store/uploadedPrefilledResponseReducer.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { PrefilledResponse } from '../models/PrefilledResponse';
+
+interface UploadedResponseState {
+  prefilledResponse: PrefilledResponse | null;
+  markedRequirements: string[];
+}
+
+const initialState: UploadedResponseState = {
+  prefilledResponse: null,
+  markedRequirements: []
+};
+
+const uploadedResponseState = createSlice({
+  name: 'uploadedResponse',
+  initialState,
+  reducers: {
+    setPrefilledResponse(state, { payload }: PayloadAction<PrefilledResponse>) {
+      state.prefilledResponse = payload;
+    },
+    setMarkedRequirements(state, { payload }: PayloadAction<string[]>) {
+      state.markedRequirements = payload;
+    }
+  }
+});
+
+export const { setPrefilledResponse } = uploadedResponseState.actions;
+
+export default uploadedResponseState.reducer;
