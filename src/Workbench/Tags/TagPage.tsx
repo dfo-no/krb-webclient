2bfb47c20ce60c55d48da3073ecbb35b3d6687a8
Trond Albinussen <tal@computas.com>
2021-11-16 15:21:07 +0100

    NestableHierarcy2 with full typing

--- src/Workbench/Tags/TagPage.tsx
+++ src/Workbench/Tags/TagPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Parentable } from '../../models/Parentable';
 import { Tag } from '../../models/Tag';
-import NestableHierarcy from '../../NestableHierarchy/NestableHierarcy';
+import NestableHierarcy2 from '../../NestableHierarchy/NestableHierarcy2';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import {
   putSelectedProjectThunk,
@@ -16,7 +16,7 @@ export default function TagPage(): React.ReactElement {
   const dispatch = useAppDispatch();
   const { project } = useAppSelector((state) => state.project);
 
-  const newTagList = (projectId: string, items: Parentable<Tag>[]) => {
+  const newTagList = (items: Parentable<Tag>[]) => {
     dispatch(setTags(items));
     dispatch(putSelectedProjectThunk('dummy'));
   };
@@ -25,12 +25,9 @@ export default function TagPage(): React.ReactElement {
     <>
       <h3 className="mt-3">{t('Tags')}</h3>
       <NewTagForm />
-      <NestableHierarcy
-        dispatchfunc={(projectId: string, items: Parentable<Tag>[]) =>
-          newTagList(projectId, items)
-        }
+      <NestableHierarcy2
+        dispatchfunc={(items: Parentable<Tag>[]) => newTagList(items)}
         inputlist={project.tags}
-        projectId={project.id}
         component={<EditTagForm element={project.tags[0]} />}
         depth={5}
       />
