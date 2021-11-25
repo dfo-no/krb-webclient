d2d0d4bba9cfe8486b5df870e955f1a0b3333dcf
imed-computas <imed@computas.com>
2021-11-25 09:29:08 +0100

    automatic update of needslist

--- src/PrefilledResponseEditor/Product/PrefilledResponseProductEditor.tsx
+++ src/PrefilledResponseEditor/Product/PrefilledResponseProductEditor.tsx
@@ -10,7 +10,6 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { FieldError, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { getPaths } from '../../common/Tree';
 import ControlledTextInput from '../../Form/ControlledTextInput';
 import ErrorSummary from '../../Form/ErrorSummary';
 import {
@@ -21,8 +20,11 @@ import { Levelable } from '../../models/Levelable';
 import { INeed } from '../../Nexus/entities/INeed';
 import { IProduct } from '../../Nexus/entities/IProduct';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { editProduct } from '../../store/reducers/PrefilledResponseReducer';
-import AnswerForm from './AnswerForm';
+import {
+  editProduct,
+  selectProduct
+} from '../../store/reducers/PrefilledResponseReducer';
+import NeedsList from './NeedsList';
 
 export default function PrefilledResponseProductEditor(): React.ReactElement {
   const { id } = useAppSelector((state) => state.selectedBank);
@@ -30,6 +32,7 @@ export default function PrefilledResponseProductEditor(): React.ReactElement {
   const { selectedProduct, prefilledResponse } = useAppSelector(
     (state) => state.prefilledResponse
   );
+
   const dispatch = useAppDispatch();
   const {
     control,
@@ -41,8 +44,6 @@ export default function PrefilledResponseProductEditor(): React.ReactElement {
     defaultValues: selectedProduct
   });
 
-  if (!id) return <p>No selected bank</p>;
-
   const addProductToSpecification = (post: IPrefilledResponseProduct) => {
     const newProduct = {
       ...post
@@ -50,40 +51,9 @@ export default function PrefilledResponseProductEditor(): React.ReactElement {
     const productIndex = prefilledResponse.products.findIndex(
       (product: IPrefilledResponseProduct) => product.id === selectedProduct.id
     );
+    dispatch(selectProduct(newProduct));
     dispatch(editProduct({ product: newProduct, productIndex }));
   };
-
-  const findNeedIdsForProduct = (productId: string, needArray: INeed[]) => {
-    const result: string[] = [];
-    needArray.forEach((need) => {
-      need.requirements.forEach((req) => {
-        req.variants.forEach((variant) => {
-          if (variant.products.includes(productId)) {
-            result.push(need.id);
-          } else {
-            for (let i = 0; i < selectedProduct.relatedProducts.length; i++) {
-              if (
-                variant.products.includes(selectedProduct.relatedProducts[i])
-              ) {
-                result.push(need.id);
-              }
-            }
-          }
-        });
-      });
-    });
-    return result;
-  };
-
-  const needIds = findNeedIdsForProduct(
-    selectedProduct.originProduct.id,
-    prefilledResponse.bank.needs
-  );
-
-  const needs = getPaths(
-    needIds,
-    prefilledResponse.bank.needs
-  ) as Levelable<INeed>[];
 
   const checkIfNeedHasRenderedAnswer = (
     need: Levelable<INeed>,
@@ -129,31 +99,6 @@ export default function PrefilledResponseProductEditor(): React.ReactElement {
     }
     return [productIsUsed, matchedProductId];
   }
-
-  const renderNeedsList = (list: Levelable<INeed>[]) => {
-    return list.map((need) => {
-      const margin = need.level === 1 ? '0rem' : `${need.level - 1}rem`;
-      const [used, productId] = checkIfNeedHasRenderedAnswerInRelatedProduct(
-        selectedProduct.relatedProducts,
-        need,
-        selectedProduct.originProduct.id
-      );
-      return (
-        <Card style={{ marginLeft: margin }} key={need.id}>
-          <Card.Header>{need.title}</Card.Header>
-          {used && (
-            <Card.Body>
-              <AnswerForm
-                element={need}
-                product={selectedProduct}
-                searchProductId={productId}
-              />
-            </Card.Body>
-          )}
-        </Card>
-      );
-    });
-  };
 
   return (
     <Container fluid>
@@ -204,7 +149,12 @@ export default function PrefilledResponseProductEditor(): React.ReactElement {
         </Card.Body>
       </Card>
       <Row className="m-4" />
-      {renderNeedsList(needs)}
+      <NeedsList
+        prefilledResponse={prefilledResponse}
+        selectedProduct={selectedProduct}
+        // eslint-disable-next-line react/jsx-no-bind
+        checkNeeds={checkIfNeedHasRenderedAnswerInRelatedProduct}
+      />
     </Container>
   );
 }
