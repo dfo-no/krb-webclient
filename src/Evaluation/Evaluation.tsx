2df1c43d160ea3b648cde69cc0c9cd6bb30971cc
imed-computas <imed@computas.com>
2021-12-06 20:14:03 +0100

    enable upload of several responses, and enable calculation of each response

--- src/Evaluation/Evaluation.tsx
+++ src/Evaluation/Evaluation.tsx
@@ -1,35 +1,52 @@
+import { AxiosResponse } from 'axios';
+import fileDownload from 'js-file-download';
 import React from 'react';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-/* import { useForm } from 'react-hook-form';
-import { Bank } from '../models/Bank';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import fileDownload from 'js-file-download'; */
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import { httpPost } from '../api/http';
+import { IResponse } from '../models/IResponse';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import {
+  setResponses,
+  setSpecification
+} from '../store/reducers/evaluation-reducer';
 
 export default function Evaluation(): React.ReactElement {
-  /*
-  const [responses, setResponses] = useState<FileDownLoad[]>([]);
+  const dispatch = useAppDispatch();
+  const { responses, specification } = useAppSelector(
+    (state) => state.evaluation
+  );
 
   const readFileContents = async (file: File) => {
+    const formData = new FormData();
+    formData.append('file', file);
     return new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-      fileReader.onerror = reject;
-      fileReader.readAsText(file);
+      httpPost<FormData, AxiosResponse>('/java/uploadPdf', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        responseType: 'json'
+      })
+        .then((response) => {
+          return resolve(response.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
   };
 
   const readAllFiles = async (AllFiles: File[]) => {
     const results = await Promise.all(
       AllFiles.map(async (file: File) => {
-        const fileContents = await readFileContents(file);
-        return JSON.parse(fileContents as string) as FileDownLoad;
+        const contents = await readFileContents(file).then((response) => {
+          return response;
+        });
+        return contents;
       })
     );
     return results;
@@ -44,91 +61,71 @@ export default function Evaluation(): React.ReactElement {
       }
       readAllFiles(allFiles)
         .then((result) => {
-          setResponses(result);
+          const newResponses = [...responses, ...(result as IResponse[])];
+          dispatch(setResponses(newResponses));
         })
         .catch((err) => {
-          // TODO: handle error gracefully
-          // eslint-disable-next-line no-alert
           alert(err);
         });
     }
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const onUploadSpecification = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const formData = new FormData();
     const files = event.target.files as FileList;
     for (let index = 0; index < files.length; index += 1) {
       const file = files[index];
-      const reader = new FileReader();
-      reader.onload = (evt) => {
-        if (evt.target?.result) {
-          const typeFileDownload = JSON.parse(
-            evt.target.result.toString()
-          ) as FileDownLoad;
-          setUploadedBank(typeFileDownload.bank);
-        }
-      };
-      reader.readAsText(file);
-      setBankFileUploaded(true);
+      formData.append('file', file);
     }
+    httpPost<FormData, AxiosResponse>('/java/uploadPdf', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      responseType: 'json'
+    }).then((response) => {
+      dispatch(setSpecification(response.data));
+      return response;
+    });
   };
 
-  if (!bankFileUploaded) {
+  if (specification.bank.id === '') {
     return (
-      <Form>
-        <Col>
-          <h4>Upload a spesification to start the evaluation</h4>
-          <InputGroup>
-            <Form.File.Input
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                handleChange(e)
-              }
-              accept=".json,application/json"
+      <Col>
+        <h4>Upload a spesification to start the evaluation</h4>
+        <InputGroup className="mb-5">
+          <form>
+            <input
+              type="file"
+              onChange={(e) => onUploadSpecification(e)}
+              name="responseFiles"
+              multiple
+              accept=".pdf"
             />
-          </InputGroup>
-        </Col>
-      </Form>
+          </form>
+        </InputGroup>
+      </Col>
     );
-  }
-
-  if (uploadedBank === null) {
-    return <p>The spec uploaded does not have the correct format</p>;
   }
 
   const onDownLoad = () => {
     const newFile = {
-      spec: uploadedBank,
+      spec: specification,
       responses
     };
-    fileDownload(
-      JSON.stringify(newFile),
-      `${uploadedBank.title}-evaluations.json`
-    );
+    fileDownload(JSON.stringify(newFile), `evaluations.json`);
   };
 
-  const calculateScore = (bank: Bank) => {
-    let score = 0;
-    bank.needs.forEach((need) => {
-      score += need.requirements.length;
-    });
-    return score;
-  };
-
-  const maxScore: number = calculateScore(uploadedBank);
-
-  const evaluations = (file: FileDownLoad[]) => {
-    const sortedResponses = [...file].sort(
-      (a, b) => calculateScore(b.bank) - calculateScore(a.bank)
-    );
-    const list = sortedResponses.map((response: FileDownLoad) => {
+  const evaluations = (iResponses: IResponse[]) => {
+    const list = iResponses.map((response: IResponse) => {
       return (
-        <Row key={response.name}>
+        <Row key={response.supplier}>
           <Col>
-            <p> {response.name} </p>
+            <p> {response.supplier} </p>
           </Col>
           <Col>
-            <p>
-              {calculateScore(response.bank)} /{maxScore} p
-            </p>
+            <p>{/* Calculate score with nexus */}</p>
           </Col>
         </Row>
       );
@@ -142,7 +139,7 @@ export default function Evaluation(): React.ReactElement {
               <hr />
             </Col>
             <Col>
-              <h6>Calculated score | Maximal score : {maxScore} </h6>
+              <h6>Calculated score : </h6>
               <hr />
             </Col>
           </Row>
@@ -150,23 +147,21 @@ export default function Evaluation(): React.ReactElement {
         </Card.Body>
       </Card>
     );
-  }; */
+  };
 
   return (
     <Container fluid>
       <Row className="m-4">
         <Col>
           <h6>Upload Responses</h6>
-          {/*
           <InputGroup className="mb-5">
             <form>
               <input
-                {...register('responseFiles')}
                 type="file"
                 onChange={(e) => handleResponseUpload(e)}
                 name="responseFiles"
                 multiple
-                accept=".json,application/json"
+                accept=".pdf"
               />
             </form>
           </InputGroup>
@@ -175,10 +170,9 @@ export default function Evaluation(): React.ReactElement {
           <Button onClick={() => onDownLoad()}>Download</Button>
         </Col>
       </Row>
+
       <Row className="m-4">
         <Col>{responses.length !== 0 && evaluations(responses)}</Col>
-          </Row> */}
-        </Col>
       </Row>
     </Container>
   );
