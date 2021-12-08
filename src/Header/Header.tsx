676ca55748fe45006a31136b9e930d434f3d218a
Christoffer Haarberg <chaa@computas.com>
2021-12-08 13:22:08 -0800

    Material UI

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,46 +1,25 @@
 import React from 'react';
-import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
-import Navbar from 'react-bootstrap/Navbar';
-import { useTranslation } from 'react-i18next';
-import { useHistory, useRouteMatch } from 'react-router-dom';
-import SignedButton from '../SignedButton/SignedButton';
-import css from './Header.module.scss';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@mui/material/Toolbar';
+import { Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 export default function Header(): React.ReactElement {
-  const history = useHistory();
-  const { t } = useTranslation();
-  const home = (): void => {
-    history.push('/');
-  };
-
-  const match = useRouteMatch({
-    path: '/workbench/:projectId',
-    strict: false,
-    sensitive: true
-  });
-
   return (
-    <Navbar bg="light" variant="dark" className={css.header}>
-      <Navbar.Brand onClick={home} role="link" className={css.header__brand}>
-        <img alt="DFØ Logo" src="/logo-blue.svg" />
-      </Navbar.Brand>
-      {match && (
-        <Button
-          variant="outline-primary"
-          onClick={() => {
-            history.push('/workbench');
-          }}
-        >
-          {t('all projects')}
-        </Button>
-      )}
-      {process.env.NODE_ENV === 'development' && (
-        <Badge bg="warning">dev</Badge>
-      )}
-      <div className={css.header__spacer} />
-
-      <SignedButton />
-    </Navbar>
+    <AppBar position="sticky">
+      <Toolbar>
+        <Grid container justifyContent="space-between">
+          <Grid item>
+            <img alt="DFØ Logo" src="/logo-blue.svg" />
+          </Grid>
+          <Grid item>
+            <Button variant="contained">Login</Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
