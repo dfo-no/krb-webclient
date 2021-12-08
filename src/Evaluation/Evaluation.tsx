780abd25dda150324e45532effea8d19c9d37115
imed-computas <73219353+imed-computas@users.noreply.github.com>
2021-12-08 10:53:34 +0100

    Krb 346 (#254)
    * small modifactions to evaluationservice and test
    
    * evaluate yes/no questions
    
    * enable upload of several responses, and enable calculation of each response
    
    * download response

--- src/Evaluation/Evaluation.tsx
+++ src/Evaluation/Evaluation.tsx
@@ -1,5 +1,7 @@
 import { AxiosResponse } from 'axios';
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import InputGroup from 'react-bootstrap/InputGroup';
