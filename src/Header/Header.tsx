c21dfaa19d38268c3189e51167eed03a04d4d617
Christoffer Haarberg <chaa@computas.com>
2021-12-08 15:27:41 -0800

    MUI Theme to separate file

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,33 +1,18 @@
 import React from 'react';
 
+import {
+  ThemeProvider,
+  createStyles,
+  makeStyles
+} from '@material-ui/core/styles';
+
 import AppBar from '@mui/material/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import Container from '@mui/material/Container';
 
-import {
-  ThemeProvider,
-  createTheme,
-  createStyles,
-  makeStyles
-} from '@material-ui/core/styles';
-
-const theme = createTheme({
-  spacing: [
-    2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
-    42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78,
-    80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100
-  ],
-  palette: {
-    primary: {
-      main: '#eb345b'
-    },
-    secondary: {
-      main: '#ffffff'
-    }
-  }
-});
+import theme from '../theme';
 
 const useStyles = makeStyles(() =>
   createStyles({
---
+++ src/theme.tsx
@@ -0,0 +1,19 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  spacing: [
+    2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
+    42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78,
+    80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100
+  ],
+  palette: {
+    primary: {
+      main: '#eb345b'
+    },
+    secondary: {
+      main: '#ffffff'
+    }
+  }
+});
+
+export default theme;
