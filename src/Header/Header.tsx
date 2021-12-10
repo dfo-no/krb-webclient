2a5402bd2619abebdbeec559c711d644d3bafa3a
Christoffer Haarberg <chaa@computas.com>
2021-12-09 18:25:39 -0800

    Theme colors

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,51 +1,37 @@
 import React from 'react';
 
-import {
-  ThemeProvider,
-  createStyles,
-  makeStyles
-} from '@material-ui/core/styles';
-
-import AppBar from '@mui/material/AppBar';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import Toolbar from '@mui/material/Toolbar';
+
 import Grid from '@material-ui/core/Grid';
 import Container from '@mui/material/Container';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+
 import theme from '../theme';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    headerContainer: {
-      marginBottom: theme.spacing(36)
-    },
-    appBar: {
-      backgroundColor: theme?.palette?.dfoBlue?.main
-    }
-  })
-);
+console.log(theme);
 
-export default function Header(): React.ReactElement {
-  const classes = useStyles();
-
+function Header() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Container className={classes.headerContainer} maxWidth="md">
-          <AppBar>
-            <Toolbar>
-              <Grid container wrap="nowrap" justifyContent="space-between">
-                <Grid item>
-                  <img alt="DFØ Logo" src="/logo-blue.svg" />
-                </Grid>
-                <Grid item>
-                  <Button variant="contained">Login</Button>
-                </Grid>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md">
+        <AppBar>
+          <Toolbar>
+            <Grid container wrap="nowrap" justifyContent="space-between">
+              <Grid item>
+                <img alt="DFØ Logo" src="/logo-blue.svg" />
               </Grid>
-            </Toolbar>
-          </AppBar>
-        </Container>
-      </ThemeProvider>
-    </>
+              <Grid item>
+                <Button>Login</Button>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Container>
+    </ThemeProvider>
   );
 }
+
+export default Header;
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,28 +1,44 @@
 import { createTheme } from '@material-ui/core/styles';
+import { PaletteOptions } from '@material-ui/core/styles/createPalette';
 
 declare module '@material-ui/core/styles/createPalette' {
-  interface Palette {
-    dfoBlue?: Palette['primary'];
-    dfoInputBackground?: Palette['primary'];
-  }
-  interface PaletteOptions {
-    dfoBlue?: PaletteOptions['primary'];
-    dfoInputBackground?: PaletteOptions['primary'];
+  export interface PaletteOptions {
+    dfoBlue: {
+      main: string;
+    };
+    dfoInputBackground: {
+      main: string;
+    };
+    dfoLightBorder: {
+      main: string;
+    };
   }
 }
 
+const colors = [
+  '#005b91' //dfoBlue
+];
+
 const theme = createTheme({
-  spacing: [
-    2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
-    42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78,
-    80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100
-  ],
   palette: {
+    primary: {
+      main: 'rgba(217,255,102,1)'
+    },
     dfoBlue: {
-      main: '#005b91'
+      main: colors[0]
     },
     dfoInputBackground: {
-      main: '#f2f1f9'
+      main: '#005b91'
+    },
+    dfoLightBorder: {
+      main: '#005b91'
+    }
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        backgroundColor: colors[0]
+      }
     }
   }
 });
