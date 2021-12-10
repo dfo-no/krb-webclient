658e4369252fadfec0455b50cc22c786041c8d31
Christoffer Haarberg <chaa@computas.com>
2021-12-10 11:01:44 -0800

    Adds DEV badge to header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
+import Badge from '@mui/material/Badge';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import { CssBaseline } from '@material-ui/core';
 
 import Grid from '@material-ui/core/Grid';
 import Container from '@mui/material/Container';
 
 import { ThemeProvider } from '@material-ui/core/styles';
+import SignedButton from '../SignedButton/SignedButton';
 
 import theme from '../theme';
 
 export default function Header(): React.ReactElement {
+  const badgeText = 'DEV';
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -31,7 +33,10 @@ export default function Header(): React.ReactElement {
                     <img alt="DFØ Logo" src="/logo-blue.svg" />
                   </Grid>
                   <Grid item>
-                    <Button>Login</Button>
+                    <Badge badgeContent={badgeText} color="secondary" />
+                  </Grid>
+                  <Grid item>
+                    <SignedButton />
                   </Grid>
                 </Grid>
               </Toolbar>
