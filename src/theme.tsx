a3a0a13144176dd2055782d5107b18cd9b09da88
Christoffer Haarberg <chaa@computas.com>
2021-12-15 10:07:39 -0800

    Removes BreakPointsOverrides type

--- src/theme.tsx
+++ src/theme.tsx
@@ -6,12 +6,6 @@ declare module '@mui/material/Button' {
   }
 }
 
-declare module '@mui/material/styles' {
-  interface BreakpointOverrides {
-    test: true;
-  }
-}
-
 declare module '@mui/material/styles/createPalette' {
   interface Palette {
     primary: PaletteColor;
