411128b6b51f392d9711b1ade5d59abe3ee7bd95
chaarberg <95414980+chaarberg@users.noreply.github.com>
2021-12-16 01:51:21 -0800

    Fixes duplicate variant and puts theme and button types in its own files. (#267)
    * Fixes duplicate variant
    
    * Separates MaterialUI Button component and Theme overrides to its own files

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,63 +1,5 @@
 import { createTheme } from '@mui/material/styles';
 
-declare module '@mui/material/Button' {
-  interface ButtonPropsVariantOverrides {
-    ordinary: true;
-  }
-}
-
-declare module '@mui/material/styles/createPalette' {
-  interface Palette {
-    primary: PaletteColor;
-    secondary: PaletteColor;
-    dfoBlue: PaletteColor;
-    dfoInputBackground: PaletteColor;
-    gray100: PaletteColor;
-    gray200: PaletteColor;
-    gray300: PaletteColor;
-    gray400: PaletteColor;
-    gray500: PaletteColor;
-    gray900: PaletteColor;
-    blue: PaletteColor;
-    indigo: PaletteColor;
-    purple: PaletteColor;
-    pink: PaletteColor;
-    red: PaletteColor;
-    yellow: PaletteColor;
-    brandPrimary: PaletteColor;
-    green: PaletteColor;
-    teal: PaletteColor;
-    cyan: PaletteColor;
-    linkHoverColor: PaletteColor;
-    success: PaletteColor;
-  }
-
-  interface PaletteOptions {
-    primary?: PaletteColorOptions;
-    secondary?: PaletteColorOptions;
-    dfoBlue: PaletteColorOptions;
-    dfoInputBackground: PaletteColorOptions;
-    gray100: PaletteColorOptions;
-    gray200: PaletteColorOptions;
-    gray300: PaletteColorOptions;
-    gray400: PaletteColorOptions;
-    gray500: PaletteColorOptions;
-    gray900: PaletteColorOptions;
-    blue: PaletteColorOptions;
-    indigo: PaletteColorOptions;
-    purple: PaletteColorOptions;
-    pink: PaletteColorOptions;
-    red: PaletteColorOptions;
-    yellow: PaletteColorOptions;
-    brandPrimary: PaletteColorOptions;
-    green: PaletteColorOptions;
-    teal: PaletteColorOptions;
-    cyan: PaletteColorOptions;
-    linkHoverColor: PaletteColorOptions;
-    success?: PaletteColorOptions;
-  }
-}
-
 const theme = createTheme({
   palette: {
     primary: {
@@ -138,13 +80,6 @@ theme.components = {
       }
     },
     variants: [
-      {
-        props: { variant: 'ordinary' },
-        style: {
-          backgroundColor: theme.palette.gray400.main,
-          whiteSpace: 'nowrap'
-        }
-      },
       {
         props: { variant: 'ordinary' },
         style: {
