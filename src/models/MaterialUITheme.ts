b7e04ee42880c20b519ee98afde7bda280e01968
Christoffer Haarberg <chaa@computas.com>
2021-12-16 10:17:57 -0800

    Separates MaterialUI Button component and Theme overrides to its own files

---
+++ src/models/MaterialUITheme.ts
@@ -0,0 +1,53 @@
+import '@mui/material/styles';
+
+declare module '@mui/material/styles/createPalette' {
+  interface Palette {
+    primary: PaletteColor;
+    secondary: PaletteColor;
+    dfoBlue: PaletteColor;
+    dfoInputBackground: PaletteColor;
+    gray100: PaletteColor;
+    gray200: PaletteColor;
+    gray300: PaletteColor;
+    gray400: PaletteColor;
+    gray500: PaletteColor;
+    gray900: PaletteColor;
+    blue: PaletteColor;
+    indigo: PaletteColor;
+    purple: PaletteColor;
+    pink: PaletteColor;
+    red: PaletteColor;
+    yellow: PaletteColor;
+    brandPrimary: PaletteColor;
+    green: PaletteColor;
+    teal: PaletteColor;
+    cyan: PaletteColor;
+    linkHoverColor: PaletteColor;
+    success: PaletteColor;
+  }
+
+  interface PaletteOptions {
+    primary?: PaletteColorOptions;
+    secondary?: PaletteColorOptions;
+    dfoBlue: PaletteColorOptions;
+    dfoInputBackground: PaletteColorOptions;
+    gray100: PaletteColorOptions;
+    gray200: PaletteColorOptions;
+    gray300: PaletteColorOptions;
+    gray400: PaletteColorOptions;
+    gray500: PaletteColorOptions;
+    gray900: PaletteColorOptions;
+    blue: PaletteColorOptions;
+    indigo: PaletteColorOptions;
+    purple: PaletteColorOptions;
+    pink: PaletteColorOptions;
+    red: PaletteColorOptions;
+    yellow: PaletteColorOptions;
+    brandPrimary: PaletteColorOptions;
+    green: PaletteColorOptions;
+    teal: PaletteColorOptions;
+    cyan: PaletteColorOptions;
+    linkHoverColor: PaletteColorOptions;
+    success?: PaletteColorOptions;
+  }
+}
