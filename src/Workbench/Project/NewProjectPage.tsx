887f98b7f70b9a7708221a7aaabdc7a1814faa12
imed <73219353+imed-computas@users.noreply.github.com>
2022-01-18 12:52:07 +0100

    new project as own page (#301)

---
+++ src/Workbench/Project/NewProjectPage.tsx
@@ -0,0 +1,90 @@
+import { joiResolver } from '@hookform/resolvers/joi';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router';
+import { v4 as uuidv4 } from 'uuid';
+import ControlledTextInput from '../../Form/ControlledTextInput';
+import ErrorSummary from '../../Form/ErrorSummary';
+import { IAlert } from '../../models/IAlert';
+import { PostProjectSchema } from '../../models/Project';
+import { IBank } from '../../Nexus/entities/IBank';
+import Nexus from '../../Nexus/Nexus';
+import { useAppDispatch } from '../../store/hooks';
+import { addAlert } from '../../store/reducers/alert-reducer';
+import {
+  postProjectThunk,
+  selectProject
+} from '../../store/reducers/project-reducer';
+
+export default function NewProjectPage(): React.ReactElement {
+  const dispatch = useAppDispatch();
+  const [validated] = useState(false);
+  const { t } = useTranslation();
+  const history = useHistory();
+  const nexus = Nexus.getInstance();
+
+  const defaultValues: IBank =
+    nexus.projectService.generateDefaultProjectValues();
+  const {
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors }
+  } = useForm<Omit<IBank, 'needs'>>({
+    resolver: joiResolver(PostProjectSchema),
+    defaultValues
+  });
+
+  const onSubmit = (post: IBank) => {
+    dispatch(postProjectThunk(post)).then((response) => {
+      const p = response.payload as IBank;
+      dispatch(selectProject(p));
+      const alert: IAlert = {
+        id: uuidv4(),
+        style: 'success',
+        text: 'Successfully  created project'
+      };
+      dispatch(addAlert({ alert }));
+      history.push(`/workbench/${p.id}`);
+    });
+  };
+
+  return (
+    <Col sm={8}>
+      <h4>Nytt prosjekt</h4>
+      <ListGroup className="mt-3">
+        <ListGroup.Item>
+          <Form
+            onSubmit={handleSubmit(onSubmit)}
+            autoComplete="off"
+            noValidate
+            validated={validated}
+          >
+            <ControlledTextInput
+              control={control}
+              name="title"
+              error={errors.title}
+              label={t('Title')}
+            />
+            <ControlledTextInput
+              control={control}
+              name="description"
+              error={errors.description}
+              label={t('Description')}
+            />
+            <Button className="mt-2" type="submit">
+              {t('save')}
+            </Button>
+
+            <ErrorSummary errors={errors} />
+          </Form>
+        </ListGroup.Item>
+      </ListGroup>
+    </Col>
+  );
+}
--- src/Workbench/Projects.tsx
+++ src/Workbench/Projects.tsx
@@ -1,62 +1,24 @@
-import { joiResolver } from '@hookform/resolvers/joi';
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
-import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { BsTrashFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import ControlledTextInput from '../Form/ControlledTextInput';
-import ErrorSummary from '../Form/ErrorSummary';
-import { IAlert } from '../models/IAlert';
-import { PostProjectSchema } from '../models/Project';
+import { Link, useHistory } from 'react-router-dom';
 import { IBank } from '../Nexus/entities/IBank';
 import Nexus from '../Nexus/Nexus';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { addAlert } from '../store/reducers/alert-reducer';
 import {
   deleteProjectThunk,
-  postProjectThunk,
   selectProject
 } from '../store/reducers/project-reducer';
 
 function Projects(): React.ReactElement {
   const dispatch = useAppDispatch();
   const { list } = useAppSelector((state) => state.project);
-  const [showNewProjectForm, setShowNewProjectForm] = useState(false);
-  const [validated] = useState(false);
   const { t } = useTranslation();
-  const nexus = Nexus.getInstance();
-
-  const defaultValues: IBank =
-    nexus.projectService.generateDefaultProjectValues();
-
-  const {
-    handleSubmit,
-    reset,
-    control,
-    formState: { errors }
-  } = useForm<Omit<IBank, 'needs'>>({
-    resolver: joiResolver(PostProjectSchema),
-    defaultValues
-  });
-
-  const onSubmit = (post: IBank) => {
-    dispatch(postProjectThunk(post)).then(() => {
-      reset();
-      setShowNewProjectForm(false);
-    });
-    const alert: IAlert = {
-      id: uuidv4(),
-      style: 'success',
-      text: 'Successfully updated product'
-    };
-    dispatch(addAlert({ alert }));
-  };
+  const history = useHistory();
 
   async function onDelete(project: IBank) {
     dispatch(deleteProjectThunk(project));
@@ -94,54 +56,15 @@ function Projects(): React.ReactElement {
     return <ListGroup className=" mt-5">{projects}</ListGroup>;
   };
 
-  function projectEditor(): JSX.Element {
-    if (showNewProjectForm) {
-      return (
-        <ListGroup className="mt-3">
-          <ListGroup.Item>
-            <Form
-              onSubmit={handleSubmit(onSubmit)}
-              autoComplete="off"
-              noValidate
-              validated={validated}
-            >
-              <ControlledTextInput
-                control={control}
-                name="title"
-                error={errors.title}
-                label={t('Title')}
-              />
-              <ControlledTextInput
-                control={control}
-                name="description"
-                error={errors.description}
-                label={t('Description')}
-              />
-              <Button variant="primary" type="submit">
-                {t('save')}
-              </Button>
-              <Button
-                variant="warning"
-                onClick={() => setShowNewProjectForm(false)}
-              >
-                {t('cancel')}
-              </Button>
-              <ErrorSummary errors={errors} />
-            </Form>
-          </ListGroup.Item>
-        </ListGroup>
-      );
-    }
-    return <></>;
-  }
-
   return (
     <>
       <h3 className="mt-3 ">{t('Projects')} </h3>
-      <Button onClick={() => setShowNewProjectForm(true)} variant="primary">
+      <Button
+        onClick={() => history.push('/workbench/project/new')}
+        variant="primary"
+      >
         {t('new project')}
       </Button>
-      {projectEditor()}
       {renderProjects(list)}
     </>
   );
