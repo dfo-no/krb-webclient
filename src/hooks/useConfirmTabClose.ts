76b773239f34df9b3c911f5962ee6adcfbf65313
Trond Albinussen <tal@computas.com>
2022-01-20 15:48:07 +0100

    Skru av tabClose når vi kjører lokalt

--- src/hooks/useConfirmTabClose.ts
+++ src/hooks/useConfirmTabClose.ts
@@ -4,13 +4,16 @@ export default function useConfirmTabClose(): void {
   const confirmationMessage = 'You have unsaved changes. Continue?';
 
   useEffect(() => {
-    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-      // eslint-disable-next-line no-param-reassign
-      event.returnValue = confirmationMessage;
-      return confirmationMessage;
-    };
+    if (window.location.origin === 'http://localhost:3000') {
+      const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+        // eslint-disable-next-line no-param-reassign
+        event.returnValue = confirmationMessage;
+        return confirmationMessage;
+      };
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
-    return () => window.removeEventListener('beforeunload', handleBeforeUnload);
+      window.addEventListener('beforeunload', handleBeforeUnload);
+      return () =>
+        window.removeEventListener('beforeunload', handleBeforeUnload);
+    }
   }, []);
 }
