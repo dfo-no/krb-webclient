d8e37789ca6706d6a59d6b65ee6a6185f5244941
chaarberg <95414980+chaarberg@users.noreply.github.com>
2022-01-27 08:45:29 +0100

    Krb 422: Lage ny header etter design sketch (#316)
    * Header, path, project data (not done), logo
    
    * .
    
    * Version date
    
    * Fixes different padding on frontpage and project page
    
    * Bigger gap between project title and version
    
    * Smaller gap small view
    
    * Finishing touch
    
    * ..
    
    * Project path
    
    * Icons
    
    * Uncomments useConfirmTabClose();
    
    * Uncomments useConfirmTabClose and reverts changes in HomePage.tsx
    
    * Reverts WorkbenchModule
    
    * Changes order of breakpoint values
    
    * Adds translation for version text
    
    * Fixes padding and gap
    
    * CSS cleanup
    
    * Adds underline in path

--- src/models/MaterialUITypographyOverrides.ts
+++ src/models/MaterialUITypographyOverrides.ts
@@ -3,22 +3,34 @@ import React from 'react';
 
 declare module '@mui/material/styles' {
   interface TypographyVariants {
+    small: React.CSSProperties;
+    smallUnderline: React.CSSProperties;
+    smallUnderlineBlue: React.CSSProperties;
+    big: React.CSSProperties;
+    bigScale: React.CSSProperties;
     footerAddressText: React.CSSProperties;
-    footerLinkText: React.CSSProperties;
     textCtrlErrorMessage: React.CSSProperties;
   }
 
   interface TypographyVariantsOptions {
+    small?: React.CSSProperties;
+    smallUnderline?: React.CSSProperties;
+    smallUnderlineBlue?: React.CSSProperties;
+    big?: React.CSSProperties;
+    bigScale?: React.CSSProperties;
     footerAddressText?: React.CSSProperties;
-    footerLinkText?: React.CSSProperties;
     textCtrlErrorMessage?: React.CSSProperties;
   }
 }
 
 declare module '@mui/material/Typography' {
   interface TypographyPropsVariantOverrides {
+    small: true;
+    smallUnderline: true;
+    smallUnderlineBlue: true;
+    big: true;
+    bigScale: true;
     footerAddressText: true;
-    footerLinkText: true;
     textCtrlErrorMessage: true;
   }
 }
