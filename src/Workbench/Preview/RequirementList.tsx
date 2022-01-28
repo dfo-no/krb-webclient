3dd2bde4ba58ffdf5fc838469f1216a04df2fcc4
imed-computas <imed@computas.com>
2022-01-28 08:28:26 +0100

    display tags on requirements that are inherited

---
+++ src/Workbench/Preview/RequirementList.tsx
@@ -0,0 +1,60 @@
+import { Button, Chip, makeStyles } from '@material-ui/core';
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { IRequirement } from '../../Nexus/entities/IRequirement';
+import theme from '../../theme';
+
+interface IProps {
+  requirements: IRequirement[];
+  updateSelectedFunction: (item: IRequirement) => void;
+}
+
+const useStyles = makeStyles({
+  requirementButton: {
+    cursor: 'pointer',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'space-between',
+    border: `1px solid ${theme.palette.lightBlue.main}`,
+    '&:hover': {
+      background: theme.palette.lightBlue.main,
+      color: theme.palette.dfoWhite.main,
+
+      '& $sideBarListItemText': {
+        color: theme.palette.dfoWhite.main
+      }
+    },
+
+    [theme.breakpoints.down('sm')]: {
+      backgroundColor: theme.palette.gray100.main
+    }
+  }
+});
+
+export default function RequirementList({
+  requirements,
+  updateSelectedFunction
+}: IProps): React.ReactElement {
+  const classes = useStyles();
+
+  const list = () => {
+    const reqList = requirements.map((element: IRequirement) => {
+      return (
+        <Button
+          onClick={() => updateSelectedFunction(element)}
+          className={classes.requirementButton}
+        >
+          {element.title}{' '}
+          {element.sourceRel && <Chip label="Arvet" variant="outlined" />}
+        </Button>
+      );
+    });
+    return (
+      <>
+        <ListGroup className="ml-3 mt-3 mb-4">{reqList}</ListGroup>
+      </>
+    );
+  };
+
+  return <>{list()}</>;
+}
