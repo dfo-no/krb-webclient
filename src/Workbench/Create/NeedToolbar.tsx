7a7f8354bfdcaf11eb85f43a1566228cd743b4e7
Trond Albinussen <tal@computas.com>
2022-02-24 10:20:17 +0100

    Can delete Needs

--- src/Workbench/Create/NeedToolbar.tsx
+++ src/Workbench/Create/NeedToolbar.tsx
@@ -1,5 +1,6 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import React from 'react';
@@ -13,15 +14,15 @@ export default function NeedToolbar(): React.ReactElement {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {need?.title}
-          </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {need?.description} Beskrivelse
-          </Typography>
-          {need && <EditNeed need={need} />}
-          {need && <DeleteNeed need={need} />}
+        <Toolbar color="secondary">
+          <Grid container>
+            <Typography variant="h6" component="span">
+              {need?.title}
+            </Typography>
+            <span style={{ flex: '1 1 auto' }}></span>
+            {need && <EditNeed need={need} />}
+            {need && <DeleteNeed need={need} />}
+          </Grid>
         </Toolbar>
       </AppBar>
     </Box>
