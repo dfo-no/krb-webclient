d0b682c9494f4335cd7130f778fe5c0113e6d08d
chaa@computas.com <chaa@computas.com>
2022-03-02 15:07:37 +0100

    FormControl SX prop

--- src/Workbench/Components/Form/FormFlexBox.tsx
+++ src/Workbench/Components/Form/FormFlexBox.tsx
@@ -3,5 +3,5 @@ import { Box, styled } from '@mui/material';
 export const FormFlexBox = styled(Box)(() => ({
   display: 'flex',
   alignSelf: 'center',
-  width: '100%'
+  flexGrow: 1
 }));
--- src/Workbench/Components/Form/FormItemBox.tsx
+++ src/Workbench/Components/Form/FormItemBox.tsx
@@ -2,6 +2,7 @@ import { Box, styled } from '@mui/material';
 
 export const FormItemBox = styled(Box)(({ theme }) => ({
   display: 'flex',
+  width: '100%',
   height: '100%',
-  width: '100%'
+  backgroundColor: theme.palette.dfoWhite.main
 }));
