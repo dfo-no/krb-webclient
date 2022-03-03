d3f92e0f319b0705fe7b79d74896cda82624cccc
Balder Riiser Haugerud <91892536+Fullstackprinsen@users.noreply.github.com>
2022-03-03 18:31:33 +0100

    krb-491: Store endringer om valg av bakgrunnsfarge for admin-sidene (#358)
    Co-authored-by: Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>

---
+++ src/components/DFOCard/DFOHeaderContentBox.tsx
@@ -0,0 +1,10 @@
+import { Box, styled } from '@mui/material';
+
+export const DFOHeaderContentBox = styled(Box)(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  padding: 15,
+  paddingLeft: 70,
+  paddingRight: 70
+}));
