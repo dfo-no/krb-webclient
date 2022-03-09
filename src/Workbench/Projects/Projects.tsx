dacd44b96b0a967a3a478779b4176f2304ee8c5a
chaa@computas.com <chaa@computas.com>
2022-03-09 16:04:12 +0100

    Design projects

--- src/Workbench/Projects/Projects.tsx
+++ src/Workbench/Projects/Projects.tsx
@@ -1,4 +1,5 @@
 import DeleteIcon from '@mui/icons-material/Delete';
+import makeStyles from '@mui/styles/makeStyles';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
@@ -21,11 +22,61 @@ import {
 import { useAppDispatch } from '../../store/hooks';
 import { addAlert } from '../../store/reducers/alert-reducer';
 import NewProject from './NewProject';
+import { StandardContainer } from '../Components/StandardContainer';
+import Card from '@mui/material/Card';
 
-function Projects(): React.ReactElement {
+import mainIllustration from '../../assets/images/main-illustration.svg';
+import {
+  NewButtonContainer,
+  SearchContainer,
+  SearchFieldContainer
+} from '../Components/SearchContainer';
+import DFOSearchBar from '../../components/DFOSearchBar/DFOSearchBar';
+import theme from '../../theme';
+
+const useStyles = makeStyles({
+  projectsContainer: {
+    backgroundColor: theme.palette.gray100.main,
+    height: '100%',
+    width: '100%',
+    paddingTop: 40
+  },
+  topContainer: {
+    display: 'flex',
+    gap: 150
+  },
+  titleSubTitleContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 15
+  },
+  projectList: {
+    backgroundColor: 'red'
+  },
+  projectListItem: { padding: 0, marginBottom: 20 },
+  projectListItemCard: {
+    width: '100%',
+    height: 100
+  },
+  projectListItemCardContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 8,
+    width: '90%',
+    paddingTop: 25,
+    paddingLeft: 25
+  },
+  projectListItemTitleButton: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+});
+
+export default function Projects(): React.ReactElement {
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const [deleteProject] = useDeleteProjectMutation();
+  const classes = useStyles();
 
   const onDelete = async (p: IBank) => {
     await deleteProject(p).then(() => {
@@ -48,57 +99,64 @@ function Projects(): React.ReactElement {
     return <p>Ingen prosjekter</p>;
   }
 
-  const renderProjects = (projectList: Record<string, IBank>) => {
-    /* TODO: does not work as we use Record now, not array. Is this ordering really neccessary?
-      Object.values(projectList)
-      .slice()
-      .sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1));
-    */
+  const list: any = [];
+  const searchFunction = () => {};
+  const callback = () => {};
 
+  const renderProjects = (projectList: Record<string, IBank>) => {
     const result = Object.values(projectList).map((element) => {
       return (
-        <div key={element.id}>
-          <ListItem alignItems="flex-start">
-            <ListItemText
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    {element.description}
-                  </Typography>
-                </React.Fragment>
-              }
-            >
-              <Link to={`/workbench/${element.id}/admin`}>{element.title}</Link>
-            </ListItemText>
-            <Button variant="warning" onClick={() => onDelete(element)}>
-              <DeleteIcon />
-            </Button>
-          </ListItem>
-          <Divider />
-        </div>
+        <ListItem className={classes.projectListItem} sx={{ width: '91%' }}>
+          <Card className={classes.projectListItemCard}>
+            <Box className={classes.projectListItemCardContent}>
+              <Box className={classes.projectListItemTitleButton}>
+                <Typography variant="smediumBold">{element.title}</Typography>
+                <DeleteIcon />
+              </Box>
+              <Divider />
+              <Typography variant="small">{element.description}</Typography>
+            </Box>
+          </Card>
+        </ListItem>
       );
     });
-    return <List sx={{ bgcolor: 'background.paper' }}>{result}</List>;
+    return <List>{result}</List>;
   };
 
   return (
-    <Container maxWidth="xl" sx={{ minHeight: '80vh' }}>
-      <Typography variant="h6">{t('Projects')}</Typography>
-      <NewProject />
-      {/* <Button
-        onClick={() => history.push('/workbench/project/new')}
-        variant="primary"
-      >
-        {t('new project')}
-      </Button> */}
-      <Box sx={{ mt: 1 }}>{renderProjects(projects)}</Box>
-    </Container>
+    <Box className={classes.projectsContainer}>
+      <StandardContainer sx={{ width: '80%' }}>
+        <Box className={classes.topContainer}>
+          <Box className={classes.titleSubTitleContainer}>
+            <Typography variant="biggerBold" sx={{ letterSpacing: 0.2 }}>
+              Velkommen til arbeidsbenken
+            </Typography>
+            <Box sx={{ width: 499 }}>
+              <Typography>
+                {t('In the workbench you can create new banks')}
+              </Typography>
+            </Box>
+          </Box>
+          <img src={mainIllustration} alt="Illustration" />
+        </Box>
+        <Box className={classes.projectsContainer}>
+          <SearchContainer sx={{ width: '90.9%' }}>
+            <SearchFieldContainer>
+              {' '}
+              <DFOSearchBar
+                list={list}
+                label={t('search for banks')}
+                callback={searchFunction}
+                searchFunction={callback}
+              />
+            </SearchFieldContainer>
+            <NewButtonContainer>
+              <Button variant="primary">{t('create new bank')}</Button>
+            </NewButtonContainer>
+          </SearchContainer>
+        </Box>
+        <Box>{renderProjects(projects)}</Box>
+      </StandardContainer>
+    </Box>
   );
 }
-
-export default Projects;
