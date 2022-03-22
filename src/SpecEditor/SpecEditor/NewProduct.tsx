b389df6632a5720c6362b55a2326b2eb24745105
chaa@computas.com <chaa@computas.com>
2022-03-22 15:21:30 +0100

    Using 100% instead of 100vw

--- src/SpecEditor/SpecEditor/NewProduct.tsx
+++ src/SpecEditor/SpecEditor/NewProduct.tsx
@@ -19,7 +19,7 @@ import NewProductNeedList from './NewProductNeedList';
 
 const useStyles = makeStyles({
   newProduct: {
-    width: '100vw',
+    width: '100%',
     height: '100%',
     backgroundColor: theme.palette.gray200.main
   },
