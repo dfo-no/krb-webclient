103be005f735e7d04a3da27a2226d3921b33ef00
Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>
2022-04-04 13:05:14 +0200

    krb-543: Små qa endringer

--- src/Workbench/Components/Form/FormContainerBox.tsx
+++ src/Workbench/Components/Form/FormContainerBox.tsx
@@ -2,8 +2,8 @@ import { Box, styled } from '@mui/material';
 
 export const FormContainerBox = styled(Box)(({ theme }) => ({
   display: 'flex',
-  height: 90,
   backgroundColor: theme.palette.white.main,
   border: `1px solid ${theme.palette.gray400.main}`,
-  marginBottom: 16
+  marginBottom: 16,
+  padding: 32
 }));
