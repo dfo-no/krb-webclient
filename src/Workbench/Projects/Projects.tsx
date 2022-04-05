d20629d2f3c40d7fc39510c72a935f4d84a66363
chaarberg <95414980+chaarberg@users.noreply.github.com>
2022-04-05 14:51:17 +0200

    Krb 560: Sette "Create" til å vises først når man går inn i arbeidsbenken. (#423)
    * Sets fixed with in SX prop on SearchFieldContainer
    
    * OnClick on delete
    
    * Changes path of Workbench ListItem onClick
    
    * Reverts SpecPage
    
    * Removes something from another pr

--- src/Workbench/Projects/Projects.tsx
+++ src/Workbench/Projects/Projects.tsx
@@ -168,6 +168,7 @@ export default function Projects(): React.ReactElement {
       }
     });
   };
+
   const searchFieldCallback = (result: Record<string, IBank>) => {
     setProjectList(result);
   };
@@ -177,7 +178,7 @@ export default function Projects(): React.ReactElement {
       return (
         <ListItem className={classes.projectListItem} key={element.id}>
           <Link
-            to={`/workbench/${element.id}/admin`}
+            to={`/workbench/${element.id}/create`}
             className={classes.projectLink}
           >
             <Card className={classes.projectListItemCard}>
