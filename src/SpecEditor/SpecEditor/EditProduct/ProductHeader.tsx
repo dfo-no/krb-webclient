9d7cadc6a8956169c51bdb743d174410ad295466
Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>
2022-05-10 15:22:44 +0200

    krb-594: Added functionality to edit button, and view of product type in spec-product header

--- src/SpecEditor/SpecEditor/EditProduct/ProductHeader.tsx
+++ src/SpecEditor/SpecEditor/EditProduct/ProductHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { DFOCardHeader } from '../../../components/DFOCard/DFOCardHeader';
 import { DFOHeaderContentBox } from '../../../components/DFOCard/DFOHeaderContentBox';
@@ -7,11 +7,14 @@ import { DFOCardHeaderIconButton } from '../../../components/DFOCard/DFOCardHead
 import { useSpecificationState } from '../../SpecificationContext';
 import EditIcon from '@mui/icons-material/Edit';
 import { useAppSelector } from '../../../store/hooks';
+import DFODialog from '../../../components/DFODialog/DFODialog';
+import EditProductForm from './EditProductForm';
 
 export default function ProductHeader(): React.ReactElement {
   const { t } = useTranslation();
   const { spec } = useAppSelector((state) => state.specification);
   const { specificationProductIndex } = useSpecificationState();
+  const [editingProduct, setEditingProduct] = useState(false);
 
   return (
     <DFOCardHeader>
@@ -32,15 +35,43 @@ export default function ProductHeader(): React.ReactElement {
           {specificationProductIndex !== -1 && (
             <DFOCardHeaderIconButton
               sx={{ marginLeft: 'auto', paddingRight: 2 }}
+              onClick={() => setEditingProduct(true)}
             >
               <EditIcon />
             </DFOCardHeaderIconButton>
           )}
         </Box>
-        <Typography variant="smBold" sx={{ paddingTop: 1 }}>
-          {specificationProductIndex !== -1 &&
-            spec.products[specificationProductIndex].description}
-        </Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'row', paddingTop: 1 }}>
+          <Typography variant="smBold">
+            {specificationProductIndex !== -1 &&
+              spec.products[specificationProductIndex].description}
+          </Typography>
+
+          {specificationProductIndex !== -1 && (
+            <Typography
+              variant="smBold"
+              sx={{ marginLeft: 'auto', paddingRight: 2 }}
+            >
+              {t('From product type')}
+              {': '}
+              <i>
+                {spec.products[specificationProductIndex].originProduct.title}
+              </i>
+            </Typography>
+          )}
+        </Box>
+        {editingProduct && (
+          <DFODialog
+            isOpen={true}
+            handleClose={() => setEditingProduct(false)}
+            children={
+              <EditProductForm
+                handleClose={() => setEditingProduct(false)}
+                specificationProduct={spec.products[specificationProductIndex]}
+              />
+            }
+          />
+        )}
       </DFOHeaderContentBox>
     </DFOCardHeader>
   );
