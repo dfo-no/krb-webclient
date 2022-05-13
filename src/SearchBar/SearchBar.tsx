56f48448387d0a50a90e9ba7c1ddf092ba3bc526
Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>
2022-05-13 15:45:42 +0200

    krb-600: Added access from homepage searchbar

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -2,10 +2,8 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useHistory } from 'react-router-dom';
 import { IBank } from '../Nexus/entities/IBank';
-import { useAppDispatch } from '../store/hooks';
-import { selectBank } from '../store/reducers/selectedBank-reducer';
+import { useBankState } from '../Home/Components/BankContext';
 
 interface IProps {
   list: Record<string, IBank>;
@@ -17,38 +15,42 @@ interface ILabel {
 }
 
 export default function SearchBar({ list }: IProps): React.ReactElement {
-  const history = useHistory();
+  const { setSelectedBank } = useBankState();
 
-  const dispatch = useAppDispatch();
   const { t } = useTranslation();
 
-  const displayList: ILabel[] = [];
-  Object.values(list).forEach((item, i) => {
-    displayList[i] = { label: item.title, value: item.id };
-  });
-
   const uniqueValuesSet = new Set();
-  const filteredArr: ILabel[] = displayList.filter((obj) => {
-    // check if name property value is already in the set
-    const isPresentInSet = uniqueValuesSet.has(obj.label);
-
-    // add name property value to Set
-    uniqueValuesSet.add(obj.label);
-    return !isPresentInSet;
-  });
+  const bankMap: Map<string, IBank> = new Map<string, IBank>();
+  const displayList: ILabel[] = Object.values(list)
+    .map((item) => {
+      bankMap.set(item.id, item);
+      return { label: item.title, value: item.id };
+    })
+    .filter((obj) => {
+      // check if name property value is already in the set
+      const isPresentInSet = uniqueValuesSet.has(obj.label);
+      // add name property value to Set
+      uniqueValuesSet.add(obj.label);
+      return !isPresentInSet;
+    });
 
   return (
     <Autocomplete
-      options={filteredArr}
+      options={displayList}
       disablePortal={true}
       onChange={(event, newValue) => {
         if (newValue) {
-          dispatch(selectBank(newValue.value));
-          history.push(`/specification/${newValue.value}`);
+          const selectedBank = bankMap.get(newValue.value);
+          if (selectedBank) {
+            setSelectedBank(selectedBank);
+          }
         }
       }}
+      isOptionEqualToValue={(option, value) => {
+        return option.label === value.label;
+      }}
       renderInput={(params) => (
-        <TextField {...params} label={t('Søk kravbanker')} />
+        <TextField {...params} label={t('search for banks')} />
       )}
     />
   );
