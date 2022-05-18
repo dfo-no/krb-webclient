8e0b43c2fc75ca258831c8cfae85edcef5f656f9
Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>
2022-05-18 09:14:32 +0200

    krb-600: Removed unused spec-page

--- src/SpecEditor/SpecModule.tsx
+++ src/SpecEditor/SpecModule.tsx
@@ -3,15 +3,11 @@ import { Route, Switch } from 'react-router';
 import SpecEditor from './SpecEditor/SpecEditor';
 import { SpecificationProvider } from './SpecificationContext';
 import SpecificationGuard from './SpecificationGuard';
-import SpecPage from './SpecPage';
 
 export default function SpecModule(): React.ReactElement {
   return (
     <SpecificationProvider>
       <Switch>
-        <Route exact path="/specification">
-          <SpecPage />
-        </Route>
         <Route exact path="/specification/:id">
           <SpecificationGuard>
             <SpecEditor />
