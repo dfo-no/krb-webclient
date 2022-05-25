14bb97299d001149912aa698dc5cd86535007725
och-compuas <69847414+och-compuas@users.noreply.github.com>
2022-05-25 13:32:57 +0200

    Krb 605 fix breadcrumbs (#464)
    * Refactored the breadcrumb and the toolbar
    
    * Resolved some linting issues
    
    * Some final clean up
    
    * Resolved some feedback from QA
    
    * Refactored tests to avoid using history
    
    Also updated packages and dependencies
    
    * Fikset any
    
    Co-authored-by: Ole Christian Helset <ole.christian.helseth@computas.com>

---
+++ src/components/DFOToolbar/DFOToolbar.test.tsx
@@ -0,0 +1,70 @@
+import React, { ReactElement } from 'react';
+import userEvent from '@testing-library/user-event';
+import { cleanup, render, RenderResult } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+
+import ConstructionOutlinedIcon from '@mui/icons-material/ConstructionOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import DFOToolbar from './DFOToolbar';
+import { IToolbarItem } from '../../models/IToolbarItem';
+
+interface MockProps {
+  location: Location;
+}
+
+const MockComponent = ({ location }: MockProps): ReactElement => {
+  const toolbarItems: IToolbarItem[] = [
+    {
+      icon: <ConstructionOutlinedIcon />,
+      label: 'Construction',
+      selected: location.pathname === '/construction',
+      url: '/construction'
+    },
+    {
+      icon: <SettingsOutlinedIcon />,
+      label: 'Settings',
+      selected: location.pathname === '/settings',
+      url: '/settings'
+    }
+  ];
+  return <DFOToolbar items={toolbarItems} />;
+};
+
+describe('DFOToolbar', () => {
+  let component: RenderResult;
+  let testLocation: Location;
+
+  beforeEach(() => {
+    component = render(
+      <MemoryRouter>
+        <Route
+          path="*"
+          render={({ location }) => {
+            testLocation = location as unknown as Location;
+            return <MockComponent location={testLocation} />;
+          }}
+        />
+      </MemoryRouter>
+    );
+  });
+
+  afterAll(cleanup);
+
+  it('Should render as expected', () => {
+    expect(component).toBeTruthy();
+    expect(component.container.querySelectorAll('a').length).toBe(2);
+    expect(component.container.querySelectorAll('a.selected').length).toBe(0);
+  });
+
+  it('Should navigate when clicking on one of the items', async () => {
+    expect(testLocation.pathname).toEqual('/');
+
+    const link = component.getByLabelText('Construction');
+    await userEvent.click(link);
+
+    expect(
+      component.container.querySelector('a.selected')?.getAttribute('href')
+    ).toEqual('/construction');
+    expect(testLocation.pathname).toEqual('/construction');
+  });
+});
