1c70c9b41f7980037aa20c8b324f9ac39ed4ccc8
Balder Riiser Haugerud <91892536+Fullstackprinsen@users.noreply.github.com>
2022-06-22 09:09:48 +0200

    krb-649: Visning av infofelt (#490)
    Co-authored-by: Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>

--- src/pages/Response/ResponseModule.tsx
+++ src/pages/Response/ResponseModule.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import { Route, Switch, useRouteMatch } from 'react-router';
 
 import ResponseEditor from './ResponseEditor';
 import { ResponseProvider } from './ResponseContext';
-import { selectBank } from '../../store/reducers/selectedBank-reducer';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
 
-interface IRouteParams {
-  bankId: string;
-}
 export default function ResponseModule(): React.ReactElement {
-  const projectMatch = useRouteMatch<IRouteParams>('/responseeditor/:bankId');
-  const dispatch = useAppDispatch();
-  const { id } = useAppSelector((state) => state.selectedBank);
-  // Can set this safely, even if we got here directly by url or by clicks
-  if (projectMatch?.params.bankId && !id) {
-    dispatch(selectBank(projectMatch?.params.bankId));
-  }
-
   return (
-    <Switch>
-      <Route exact path="/response/:id">
-        <ResponseProvider>
-          <ResponseEditor />
-        </ResponseProvider>
-      </Route>
-    </Switch>
+    <ResponseProvider>
+      <ResponseEditor />
+    </ResponseProvider>
   );
 }
