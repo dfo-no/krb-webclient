014123bdfc3d5c127022cd14ba91545f81d33c19
Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>
2022-06-27 13:13:25 +0200

    Revert "krb-651: Oppdatering etter qa"
    This reverts commit 9eefbb428c6eb3c782136337298310117d4cb7e8.

--- src/pages/Response/Answer/QuestionAnswer/ProductQuestionAnswer.tsx
+++ src/pages/Response/Answer/QuestionAnswer/ProductQuestionAnswer.tsx
@@ -6,13 +6,7 @@ import QuestionAnswerPeriodDate from './QuestionAnswerPeriodDate';
 import QuestionAnswerSlider from './QuestionAnswerSlider';
 import QuestionAnswerText from './QuestionAnswerText';
 import QuestionAnswerTime from './QuestionAnswerTime';
-import { ICheckboxQuestion } from '../../../../Nexus/entities/ICheckboxQuestion';
-import { ICodelistQuestion } from '../../../../Nexus/entities/ICodelistQuestion';
-import { IPeriodDateQuestion } from '../../../../Nexus/entities/IPeriodDateQuestion';
 import { IRequirementAnswer } from '../../../../models/IRequirementAnswer';
-import { ISliderQuestion } from '../../../../Nexus/entities/ISliderQuestion';
-import { ITextQuestion } from '../../../../Nexus/entities/ITextQuestion';
-import { ITimeQuestion } from '../../../../Nexus/entities/ITimeQuestion';
 import { QuestionVariant } from '../../../../enums';
 
 interface IProps {
@@ -24,61 +18,86 @@ export default function ProductQuestionAnswer({
   requirementAnswer,
   existingAnswer
 }: IProps): React.ReactElement {
-  if (
-    existingAnswer &&
-    existingAnswer.question.type === requirementAnswer.question.type
-  ) {
-    switch (requirementAnswer.question.type) {
-      case QuestionVariant.Q_CHECKBOX:
-        return (
-          <QuestionAnswerCheckbox
-            item={requirementAnswer.question}
-            parent={requirementAnswer}
-            existingAnswer={existingAnswer.question as ICheckboxQuestion}
-          />
-        );
-      case QuestionVariant.Q_SLIDER:
-        return (
-          <QuestionAnswerSlider
-            item={requirementAnswer.question}
-            parent={requirementAnswer}
-            existingAnswer={existingAnswer.question as ISliderQuestion}
-          />
-        );
-      case QuestionVariant.Q_TEXT:
-        return (
-          <QuestionAnswerText
-            item={requirementAnswer.question}
-            parent={requirementAnswer}
-            existingAnswer={existingAnswer.question as ITextQuestion}
-          />
-        );
-      case QuestionVariant.Q_CODELIST:
-        return (
-          <QuestionAnswerCodelist
-            item={requirementAnswer.question}
-            parent={requirementAnswer}
-            existingAnswer={existingAnswer.question as ICodelistQuestion}
-          />
-        );
-      case QuestionVariant.Q_PERIOD_DATE:
-        return (
-          <QuestionAnswerPeriodDate
-            item={requirementAnswer.question}
-            parent={requirementAnswer}
-            existingAnswer={existingAnswer.question as IPeriodDateQuestion}
-          />
-        );
-      case QuestionVariant.Q_TIME:
-        return (
-          <QuestionAnswerTime
-            item={requirementAnswer.question}
-            parent={requirementAnswer}
-            existingAnswer={existingAnswer.question as ITimeQuestion}
-          />
-        );
-      case QuestionVariant.Q_FILEUPLOAD:
-    }
+  switch (requirementAnswer.question.type) {
+    case QuestionVariant.Q_CHECKBOX:
+      return (
+        <QuestionAnswerCheckbox
+          item={requirementAnswer.question}
+          parent={requirementAnswer}
+          existingAnswer={
+            existingAnswer &&
+            existingAnswer.question.type === QuestionVariant.Q_CHECKBOX
+              ? existingAnswer.question
+              : undefined
+          }
+        />
+      );
+    case QuestionVariant.Q_SLIDER:
+      return (
+        <QuestionAnswerSlider
+          item={requirementAnswer.question}
+          parent={requirementAnswer}
+          existingAnswer={
+            existingAnswer &&
+            existingAnswer.question.type === QuestionVariant.Q_SLIDER
+              ? existingAnswer.question
+              : undefined
+          }
+        />
+      );
+    case QuestionVariant.Q_TEXT:
+      return (
+        <QuestionAnswerText
+          item={requirementAnswer.question}
+          parent={requirementAnswer}
+          existingAnswer={
+            existingAnswer &&
+            existingAnswer.question.type === QuestionVariant.Q_TEXT
+              ? existingAnswer.question
+              : undefined
+          }
+        />
+      );
+    case QuestionVariant.Q_CODELIST:
+      return (
+        <QuestionAnswerCodelist
+          item={requirementAnswer.question}
+          parent={requirementAnswer}
+          existingAnswer={
+            existingAnswer &&
+            existingAnswer.question.type === QuestionVariant.Q_CODELIST
+              ? existingAnswer.question
+              : undefined
+          }
+        />
+      );
+    case QuestionVariant.Q_PERIOD_DATE:
+      return (
+        <QuestionAnswerPeriodDate
+          item={requirementAnswer.question}
+          parent={requirementAnswer}
+          existingAnswer={
+            existingAnswer &&
+            existingAnswer.question.type === QuestionVariant.Q_PERIOD_DATE
+              ? existingAnswer.question
+              : undefined
+          }
+        />
+      );
+    case QuestionVariant.Q_TIME:
+      return (
+        <QuestionAnswerTime
+          item={requirementAnswer.question}
+          parent={requirementAnswer}
+          existingAnswer={
+            existingAnswer &&
+            existingAnswer.question.type === QuestionVariant.Q_TIME
+              ? existingAnswer.question
+              : undefined
+          }
+        />
+      );
+    case QuestionVariant.Q_FILEUPLOAD:
   }
   return <></>;
 }
