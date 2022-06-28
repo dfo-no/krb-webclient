3a30c0b97e3b48d2cd9f079880f45de604fb36ba
Balder Riiser Haugerud <balder.riiser.haugerud@computas.com>
2022-06-28 14:49:37 +0200

    krb-671: Oppdatering etter qa

--- src/Nexus/services/EvaluationService.ts
+++ src/Nexus/services/EvaluationService.ts
@@ -1,7 +1,20 @@
 import { IEvaluatedResponse } from '../entities/IEvaluatedResponse';
+import { IPointsCalculation } from '../entities/IPointsCalculation';
+import { IRequirementAnswer } from '../../models/IRequirementAnswer';
 import { IResponse } from '../../models/IResponse';
+import { IResponseProduct } from '../../models/IResponseProduct';
 
 export default class EvaluationService {
+  private static calculatePoints(
+    requirementAnswer: IRequirementAnswer
+  ): IPointsCalculation {
+    const weight = requirementAnswer.weight / 100;
+    const points = requirementAnswer.question.answer.point;
+    return !!points
+      ? { total: points * weight, max: 100 * weight }
+      : { total: 0, max: 100 * weight };
+  }
+
   async evaluateAll(responses: IResponse[]): Promise<IEvaluatedResponse[]> {
     const evaluations: IEvaluatedResponse[] = [];
     responses.forEach((response) => {
@@ -9,6 +22,17 @@ export default class EvaluationService {
       evaluations.push(result);
     });
     return evaluations;
+  }
+
+  calculateProductPoints(product: IResponseProduct): IPointsCalculation {
+    let total = 0;
+    let max = 0;
+    product.requirementAnswers.forEach((requirementAnswer) => {
+      const calc = EvaluationService.calculatePoints(requirementAnswer);
+      total += calc.total;
+      max += calc.max;
+    });
+    return max === 0 ? { total: 0, max: 0 } : { total, max };
   }
 
   evaluate(response: IResponse): IEvaluatedResponse {
@@ -20,23 +44,16 @@ export default class EvaluationService {
     let totPoints = 0;
 
     response.requirementAnswers.forEach((requirementAnswer) => {
-      const weight = requirementAnswer.weight / 100;
-      const points = requirementAnswer.question.answer.point;
-      if (points) {
-        totPoints += points * weight;
-      }
-      maxPoints += 100 * weight;
+      const calc = EvaluationService.calculatePoints(requirementAnswer);
+      totPoints += calc.total;
+      maxPoints += calc.max;
     });
 
     response.products.forEach((product) => {
-      product.requirementAnswers.forEach((requirementAnswer) => {
-        const weight = requirementAnswer.weight / 100;
-        const points = requirementAnswer.question.answer.point;
-        if (points) {
-          totPoints += points * weight;
-        }
-        maxPoints += 100 * weight;
-      });
+      // TODO: Add product weighting
+      const calc = this.calculateProductPoints(product);
+      totPoints += calc.total;
+      maxPoints += calc.max;
     });
 
     evaluation.points = totPoints / maxPoints;
