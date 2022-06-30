f8f4759b9a69a7707a959812e40bd1edef78e5f6
Ole Christian Helset <ole.christian.helseth@computas.com>
2022-06-30 13:01:51 +0200

    Fikset slik at man ikke ser "Lag ditt første produkt" når man har

--- src/pages/SpecEditor/SpecEditor.tsx
+++ src/pages/SpecEditor/SpecEditor.tsx
@@ -1,15 +1,36 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 
 import css from '../Stylesheets/Editor.module.scss';
 import EditProduct from './EditProduct/EditProduct';
 import NewProduct from './NewProduct/NewProduct';
 import NoProducts from './NoProducts/NoProducts';
 import SpecSideBar from './SideBar/SpecSideBar';
+import { useAppSelector } from '../../store/hooks';
 import { useSpecificationState } from './SpecificationContext';
 
-export default function SpecEditor(): React.ReactElement {
-  const { specificationProductIndex, genericRequirement, create } =
-    useSpecificationState();
+export default function SpecEditor(): ReactElement {
+  const { spec } = useAppSelector((state) => state.specification);
+  const {
+    create,
+    genericRequirement,
+    setGenericRequirement,
+    specificationProductIndex
+  } = useSpecificationState();
+
+  useEffect(() => {
+    if (
+      !genericRequirement &&
+      specificationProductIndex === -1 &&
+      spec.products.length
+    ) {
+      setGenericRequirement(true);
+    }
+  }, [
+    genericRequirement,
+    setGenericRequirement,
+    spec,
+    specificationProductIndex
+  ]);
 
   const renderProduct = (): ReactElement => {
     if (create) {
