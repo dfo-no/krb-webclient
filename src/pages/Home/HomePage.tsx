a20254a801128d6e2d04fb7db737fe490443f95c
Erik Vårdal Itland <erik.vardal.itland@entur.org>
2022-09-21 17:54:48 +0200

    KRB-682 Trekk ut HomePageUpload.tsx

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,51 +1,26 @@
 import classnames from 'classnames';
 import React, { useEffect, useState } from 'react';
-import { AxiosResponse } from 'axios';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { v4 as uuidv4 } from 'uuid';
 
 import css from './HomePage.module.scss';
-import FileUpload from '../../components/FileUpload/FileUpload';
 import Footer from '../../Footer/Footer';
 import HomeDisplayList from './HomeDisplayList';
 import HomeSearchBar from './HomeSearchBar';
-import PrefilledResponseSelectionModal from './PrefilledResponseSelectionModal';
-import ProjectSelectionModal from './ProjectSelectionModal';
-import ResponseSelectionModal from './ResponseSelectionModal';
-import SpecificationSelectionModal from './SpecificationSelectionModal';
-import { addAlert } from '../../store/reducers/alert-reducer';
-import { httpPost } from '../../api/http';
-import { IAlert } from '../../models/IAlert';
 import { IBank } from '../../Nexus/entities/IBank';
-import { useAppDispatch } from '../../store/hooks';
 import { useGetBanksQuery } from '../../store/api/bankApi';
-import { ISpecification } from '../../Nexus/entities/ISpecification';
-import { IResponse } from '../../Nexus/entities/IResponse';
-import { IPrefilledResponse } from '../../Nexus/entities/IPrefilledResponse';
-import { IFile } from '../../models/IFile';
+
+import { HomePageUpload } from './HomePageUpload';
 
 export default function HomePage(): React.ReactElement {
-  const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const [selectedBank, setSelectedBank] = useState<IBank | null>(null);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [specFile, setSpecFile] = useState<IFile | null>(null); // TODO: Fix
-  const [selectedSpecification, setSelectedSpecification] =
-    useState<ISpecification | null>(null);
-  const [selectedResponse, setSelectedResponse] = useState<IResponse | null>(
-    null
-  );
-  const [selectedPrefilledResponse, setSelectedPrefilledResponse] =
-    useState<IPrefilledResponse | null>(null);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [xyz, setXys] = useState<ISpecification | null>(null); // TODO: This should go away
 
   const [latestPublishedProjects, setLatestPublishedProjects] = useState<
     IBank[]
   >([]);
 
-  const { data: list } = useGetBanksQuery({
+  const { data: listOfBanks } = useGetBanksQuery({
     pageSize: 500,
     page: 1,
     fieldName: 'title',
@@ -53,9 +28,9 @@ export default function HomePage(): React.ReactElement {
   });
 
   useEffect(() => {
-    if (list) {
+    if (listOfBanks) {
       const latestPublishedMap = new Map<string, IBank>();
-      Object.values(list).forEach((bank) => {
+      Object.values(listOfBanks).forEach((bank) => {
         if (!bank.publishedDate || !bank.projectId) {
           return;
         }
@@ -70,66 +45,7 @@ export default function HomePage(): React.ReactElement {
       });
       setLatestPublishedProjects(Array.from(latestPublishedMap.values()));
     }
-  }, [list]);
-
-  const onUpload = (files: FileList): void => {
-    const MAX_UPLOAD_SIZE = 10000000; // 10M
-
-    const formData = new FormData();
-    let disableUploadMessage = '';
-    for (let index = 0; index < files.length; index += 1) {
-      const file = files[index];
-      if (file.size > MAX_UPLOAD_SIZE) {
-        disableUploadMessage = t('HOME_FILEUPL_TOO_LARGE');
-        break;
-      }
-      if (file.type !== 'application/pdf') {
-        disableUploadMessage = t('HOME_FILEUPL_WRONG_TYPE');
-        break;
-      }
-      formData.append('file', file);
-    }
-
-    if (disableUploadMessage !== '') {
-      const alert: IAlert = {
-        id: uuidv4(),
-        style: 'error',
-        text: disableUploadMessage
-      };
-      dispatch(addAlert({ alert }));
-      return;
-    }
-
-    httpPost<FormData, AxiosResponse>('/java/uploadPdf', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-      responseType: 'json'
-    })
-      .then((httpResponse) => {
-        if (httpResponse.data.title) {
-          setSpecFile({
-            name: files[0].name,
-            lastModified: files[0].lastModified
-          });
-          setSelectedSpecification(httpResponse.data);
-        } else {
-          if (!httpResponse.data.specification) {
-            setSelectedPrefilledResponse(httpResponse.data);
-          } else {
-            setSelectedResponse(httpResponse.data);
-          }
-        }
-      })
-      .catch(() => {
-        const alert: IAlert = {
-          id: uuidv4(),
-          style: 'error',
-          text: t('HOME_FILEUPL_UPLOAD_ERROR')
-        };
-        dispatch(addAlert({ alert }));
-      });
-  };
+  }, [listOfBanks]);
 
   return (
     <div className={css.HomePage}>
@@ -148,13 +64,9 @@ export default function HomePage(): React.ReactElement {
                 <span>{t('Create projects')}</span>
               </Link>
             </div>
-            <FileUpload
-              accept={'application/pdf'}
-              className={css.Card}
-              description={t('HOME_FILEUPL_DESCRIPTION')}
-              label={t('HOME_FILEUPL_LABEL')}
-              onChange={onUpload}
-              variant={'Tertiary'}
+            <HomePageUpload
+              selectedBank={selectedBank}
+              setSelectedBank={setSelectedBank}
             />
           </div>
         </div>
@@ -173,31 +85,6 @@ export default function HomePage(): React.ReactElement {
         </div>
       </div>
       <Footer />
-      {selectedBank && (
-        <ProjectSelectionModal
-          selectedBank={selectedBank}
-          setSelectedBank={setSelectedBank}
-        />
-      )}
-      {selectedSpecification && (
-        <SpecificationSelectionModal
-          selectedSpecification={selectedSpecification}
-          setSelectedSpecification={setSelectedSpecification}
-          setEvaluationSpecification={setXys}
-        />
-      )}
-      {selectedResponse && (
-        <ResponseSelectionModal
-          selectedResponse={selectedResponse}
-          setSelectedResponse={setSelectedResponse}
-        />
-      )}
-      {selectedPrefilledResponse && (
-        <PrefilledResponseSelectionModal
-          selectedPrefilledResponse={selectedPrefilledResponse}
-          setSelectedPrefilledResponse={setSelectedPrefilledResponse}
-        />
-      )}
     </div>
   );
 }
---
+++ src/pages/Home/HomePageUpload.tsx
@@ -0,0 +1,138 @@
+import css from './HomePage.module.scss';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { AxiosResponse } from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+
+import ProjectSelectionModal from './ProjectSelectionModal';
+import SpecificationSelectionModal from './SpecificationSelectionModal';
+import ResponseSelectionModal from './ResponseSelectionModal';
+import PrefilledResponseSelectionModal from './PrefilledResponseSelectionModal';
+import FileUpload from '../../components/FileUpload/FileUpload';
+import { IBank } from '../../Nexus/entities/IBank';
+import { ISpecification } from '../../Nexus/entities/ISpecification';
+import { IResponse } from '../../Nexus/entities/IResponse';
+import { IPrefilledResponse } from '../../Nexus/entities/IPrefilledResponse';
+import { IFile } from '../../models/IFile';
+import { IAlert } from '../../models/IAlert';
+import { addAlert } from '../../store/reducers/alert-reducer';
+import { httpPost } from '../../api/http';
+import { useAppDispatch } from '../../store/hooks';
+
+type Props = {
+  selectedBank: IBank | null;
+  setSelectedBank: Dispatch<SetStateAction<IBank | null>>;
+};
+
+export function HomePageUpload({ selectedBank, setSelectedBank }: Props) {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [specFile, setSpecFile] = useState<IFile | null>(null); // TODO: Fix
+  const [selectedSpecification, setSelectedSpecification] =
+    useState<ISpecification | null>(null);
+  const [selectedResponse, setSelectedResponse] = useState<IResponse | null>(
+    null
+  );
+  const [selectedPrefilledResponse, setSelectedPrefilledResponse] =
+    useState<IPrefilledResponse | null>(null);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [xyz, setXys] = useState<ISpecification | null>(null); // TODO: This should go away
+
+  const onUpload = (files: FileList): void => {
+    const MAX_UPLOAD_SIZE = 10000000; // 10M
+
+    const formData = new FormData();
+    let disableUploadMessage = '';
+    for (let index = 0; index < files.length; index += 1) {
+      const file = files[index];
+      if (file.size > MAX_UPLOAD_SIZE) {
+        disableUploadMessage = t('HOME_FILEUPL_TOO_LARGE');
+        break;
+      }
+      if (file.type !== 'application/pdf') {
+        disableUploadMessage = t('HOME_FILEUPL_WRONG_TYPE');
+        break;
+      }
+      formData.append('file', file);
+    }
+
+    if (disableUploadMessage !== '') {
+      const alert: IAlert = {
+        id: uuidv4(),
+        style: 'error',
+        text: disableUploadMessage
+      };
+      dispatch(addAlert({ alert }));
+      return;
+    }
+
+    httpPost<FormData, AxiosResponse>('/java/uploadPdf', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      responseType: 'json'
+    })
+      .then((httpResponse) => {
+        if (httpResponse.data.title) {
+          setSpecFile({
+            name: files[0].name,
+            lastModified: files[0].lastModified
+          });
+          setSelectedSpecification(httpResponse.data);
+        } else {
+          if (!httpResponse.data.specification) {
+            setSelectedPrefilledResponse(httpResponse.data);
+          } else {
+            setSelectedResponse(httpResponse.data);
+          }
+        }
+      })
+      .catch(() => {
+        const alert: IAlert = {
+          id: uuidv4(),
+          style: 'error',
+          text: t('HOME_FILEUPL_UPLOAD_ERROR')
+        };
+        dispatch(addAlert({ alert }));
+      });
+  };
+
+  return (
+    <>
+      <FileUpload
+        accept={'application/pdf'}
+        className={css.Card}
+        description={t('HOME_FILEUPL_DESCRIPTION')}
+        label={t('HOME_FILEUPL_LABEL')}
+        onChange={onUpload}
+        variant={'Tertiary'}
+      />
+      {selectedBank && (
+        <ProjectSelectionModal
+          selectedBank={selectedBank}
+          setSelectedBank={setSelectedBank}
+        />
+      )}
+      {selectedSpecification && (
+        <SpecificationSelectionModal
+          selectedSpecification={selectedSpecification}
+          setSelectedSpecification={setSelectedSpecification}
+          setEvaluationSpecification={setXys}
+        />
+      )}
+      {selectedResponse && (
+        <ResponseSelectionModal
+          selectedResponse={selectedResponse}
+          setSelectedResponse={setSelectedResponse}
+        />
+      )}
+      {selectedPrefilledResponse && (
+        <PrefilledResponseSelectionModal
+          selectedPrefilledResponse={selectedPrefilledResponse}
+          setSelectedPrefilledResponse={setSelectedPrefilledResponse}
+        />
+      )}
+    </>
+  );
+}
