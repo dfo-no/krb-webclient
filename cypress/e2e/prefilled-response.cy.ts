deb2b05551b2e729cc2b1c677b15e65794c35d93
erik-itland <evi@computas.com>
2022-09-28 09:42:13 +0200

    KRB-682 Legg inn beskrivelser siden testen er lang

--- cypress/e2e/prefilled-response.cy.ts
+++ cypress/e2e/prefilled-response.cy.ts
@@ -5,12 +5,16 @@ describe('create prefilled response', () => {
     cy.contains('Søk etter').parent().click().type('Kjøretøy til hjemmetjenesten')
     cy.contains('Kjøretøy til hjemmetjenesten').click()
     cy.contains('Kjøretøy til hjemmetjenesten')
+
+    // Her starter "Lag forberedt besvarelse"
     cy.contains('Lag forberedt besvarelse').click()
     cy.get('input[placeholder="Navn på leverandør"]').type('leverandør e77f5faf-9b3f-49ba-b216-d9951c11ab11')
     cy.contains('Lag forberedt besvarelse').click()
     cy.url().should('include', 'http://localhost:3000/prefilledresponse/')
     cy.contains('Kjøretøy til hjemmetjenesten')
     cy.contains('Last ned forberedt besvarelse')
+
+    // Her legger vi til et nytt produkt og sjekker at alt ser riktig ut
     cy.contains('Lag et nytt produkt').click()
     cy.get('input[placeholder="Navn på produkt"]').type('bil 5783d780-d4a4-49aa-90e9-1b2f3ef86da3')
     cy.get('input[placeholder="Beskrivelse av produktet"]').type('enkel elbil')
@@ -20,6 +24,8 @@ describe('create prefilled response', () => {
     cy.contains('Bagasjerom')
     cy.contains('Sikring')
     cy.contains('Utseende')
+
+    // Her endrer vi produkttype til El-bil og ser at beskrivelsen av krav endrer seg
     cy.contains('El-bil').click()
     cy.should('not.contain', 'Krav du finner under Bil')
     cy.contains('Krav du finner under El-bil')
@@ -28,7 +34,11 @@ describe('create prefilled response', () => {
     cy.contains('Bagasjerom')
     cy.contains('Sikring')
     cy.contains('Utseende')
+
+    // Her lagrer vi det nye produktet
     cy.get('button').contains('Lagre').click()
+
+    // Her setter vi og lagrer informasjon om de to første kravene
     cy.contains('Tilbehør i kupé').parent().contains('Ikke besvart').click()
     cy.contains('Tilbehør i kupé').parent().contains('Lagre').click()
     cy.contains('Tilbehør i kupé').parent().should('not.contain', 'Ikke besvart').click()
