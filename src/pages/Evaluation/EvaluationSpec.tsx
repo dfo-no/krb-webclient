90787a7e6d677768c1b7e7dd8086970d82ef686d
erik-itland <evi@computas.com>
2022-09-30 13:01:25 +0200

    KRB-682 Fiks utkommentert opplasting i EvaluationSpec.tsx

--- src/pages/Evaluation/EvaluationSpec.tsx
+++ src/pages/Evaluation/EvaluationSpec.tsx
@@ -1,25 +1,30 @@
-import React, { ReactElement } from 'react';
-// import { AxiosResponse } from 'axios';
-// import { useTranslation } from 'react-i18next';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { AxiosResponse } from 'axios';
+import { useTranslation } from 'react-i18next';
 
 import css from './Evaluation.module.scss';
 import DateUtils from '../../common/DateUtils';
-// import FileUpload from '../../components/FileUpload/FileUpload';
-// import SpecificationService from '../../Nexus/services/SpecificationService';
-// import { httpPost } from '../../api/http';
-// import { useAppDispatch } from '../../store/hooks';
+import FileUpload from '../../components/FileUpload/FileUpload';
+import { httpPost } from '../../api/http';
+import { useAppDispatch } from '../../store/hooks';
 import { useEvaluationState } from './EvaluationContext';
+import {
+  getDefaultSpecificationFile,
+  getInvalidSpecificationFile,
+  INVALID_ID
+} from '../../Nexus/services/EvaluationSpecificationStoreService';
 
 const EvaluationSpec = (): ReactElement => {
-  // const { t } = useTranslation();
-  // const [uploadError, setUploadError] = useState('');
-  // const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+  const [uploadError, setUploadError] = useState('');
+  const dispatch = useAppDispatch();
 
-  // useEffect(() => {
-  //   setUploadError('');
-  // }, [dispatch]);
+  useEffect(() => {
+    setUploadError('');
+  }, [dispatch]);
 
-  const { specificationUpload } = useEvaluationState();
+  const { setEvaluations, setSpecificationUpload, specificationUpload } =
+    useEvaluationState();
 
   const formatDate = (time: number): string => {
     const date = new Date(time);
@@ -27,7 +32,10 @@ const EvaluationSpec = (): ReactElement => {
   };
 
   const getSpecificationName = (): string => {
-    if (specificationUpload.specification.bank.id === '') {
+    if (
+      specificationUpload.id === INVALID_ID ||
+      specificationUpload.specification.bank.id === ''
+    ) {
       return '...';
     }
 
@@ -38,56 +46,50 @@ const EvaluationSpec = (): ReactElement => {
     );
   };
 
-  // const onUploadSpecification = (fileList: FileList): void => {
-  //   setUploadError('');
-  //   setEvaluations([]);
-  //
-  //   const formData = new FormData();
-  //   if (fileList.length) {
-  //     const file = fileList[0];
-  //     setSpecFile({
-  //       name: file.name,
-  //       lastModified: file.lastModified
-  //     });
-  //     formData.append('file', file);
-  //   }
-  //
-  //   httpPost<FormData, AxiosResponse>('/java/uploadPdf', formData, {
-  //     headers: {
-  //       'Content-Type': 'multipart/form-data'
-  //     },
-  //     responseType: 'json'
-  //   })
-  //     .then((response) => {
-  //       if (!response.data.bank) {
-  //         setUploadError(t('EVAL_SPEC_ERROR_INVALID_FILE'));
-  //
-  //         setEvaluationSpecification(
-  //           SpecificationService.defaultSpecification()
-  //         );
-  //         return response;
-  //       }
-  //       setEvaluationSpecification(response.data);
-  //       return response;
-  //     })
-  //     .catch((error) => {
-  //       setSpecFile(null);
-  //       setUploadError(t('EVAL_SPEC_ERROR_UPLOADING'));
-  //       console.error(error);
-  //     });
-  // };
+  const onUploadSpecification = (fileList: FileList): void => {
+    setUploadError('');
+    setEvaluations([]);
+
+    const formData = new FormData();
+    if (fileList.length) {
+      const file = fileList[0];
+      formData.append('file', file);
+      httpPost<FormData, AxiosResponse>('/java/uploadPdf', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        responseType: 'json'
+      })
+        .then((response) => {
+          if (!response.data.bank) {
+            setUploadError(t('EVAL_SPEC_ERROR_INVALID_FILE'));
+
+            setSpecificationUpload(getInvalidSpecificationFile());
+            return response;
+          }
+          setSpecificationUpload(
+            getDefaultSpecificationFile(file, response.data)
+          );
+          return response;
+        })
+        .catch((error) => {
+          setUploadError(t('EVAL_SPEC_ERROR_UPLOADING'));
+          console.error(error);
+        });
+    }
+  };
 
   return (
     <div className={css.Content}>
       <div className={css.Card}>
-        {/* <FileUpload
+        <FileUpload
           accept={'application/pdf'}
           description={t('EVAL_SPEC_FILE_UPL_DESCR')}
           label={t('EVAL_SPEC_FILE_UPL_LABEL')}
           onChange={onUploadSpecification}
           variant={'Tertiary'}
-        />*/}
-        {specificationUpload !== null && (
+        />
+        {specificationUpload.specification !== null && (
           <ul className={css.Files}>
             <li className={css.File}>
               <div>{getSpecificationName()}</div>
@@ -101,7 +103,7 @@ const EvaluationSpec = (): ReactElement => {
           </ul>
         )}
       </div>
-      {/* {!!uploadError && <div className={css.Error}>{uploadError}</div>}*/}
+      {!!uploadError && <div className={css.Error}>{uploadError}</div>}
     </div>
   );
 };
