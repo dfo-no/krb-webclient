bfbeb50c793211218881a35a8ef0ebe5d497113c
Raghad Al-abd <82160821+RaghadAL-Abd@users.noreply.github.com>
2022-09-30 10:19:17 +0200

    krb-734 Aktivt valg for ikke-akseptert svar på bekreftelse (byggeren) (#575)

--- src/pages/SpecEditor/EditProduct/QuestionSpecification/QuestionSpecificationConfirmation.tsx
+++ src/pages/SpecEditor/EditProduct/QuestionSpecification/QuestionSpecificationConfirmation.tsx
@@ -1,20 +1,51 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { useFormContext, useWatch } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { Typography } from '@mui/material';
 
 import css from '../QuestionContent.module.scss';
 import VerticalTextCtrl from '../../../../FormProvider/VerticalTextCtrl';
+import { IRequirementAnswer } from '../../../../Nexus/entities/IRequirementAnswer';
+import { DFOCheckbox } from '../../../../components/DFOCheckbox/DFOCheckbox';
+
 const QuestionSpecificationConfirmation = (): ReactElement => {
   const { t } = useTranslation();
+  const [preferredScore, setPreferredScore] = useState(false);
+  const { control, setValue } = useFormContext<IRequirementAnswer>();
+  const usePointsNonPrefered = useWatch({
+    name: 'question.config.pointsUnconfirmed',
+    control
+  });
+
+  useEffect(() => {
+    if (usePointsNonPrefered > 0) {
+      setPreferredScore(true);
+    }
+  }, [usePointsNonPrefered]);
+
+  const onCheckboxClick = (): void => {
+    if (preferredScore) {
+      setValue('question.config.pointsUnconfirmed', 0);
+    }
+    setPreferredScore((prev) => !prev);
+  };
+
   return (
     <div className={css.QuestionFlex}>
-      <div>
+      <div onClick={onCheckboxClick}>
+        <DFOCheckbox checked={preferredScore} />
+        <Typography className={css.CheckboxLabel} variant={'smBold'}>
+          {t('Give score for non-accepted alternative')}
+        </Typography>
+      </div>
+      {preferredScore && (
         <VerticalTextCtrl
           name={'question.config.pointsUnconfirmed'}
           label={t('Score for unconfirmed')}
           placeholder={''}
           type={'number'}
         />
-      </div>
+      )}
     </div>
   );
 };
