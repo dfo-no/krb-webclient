1a0f977c4efe0187bd0646b7fa8481b5cd2ad0ac
erik-itland <evi@computas.com>
2022-09-30 15:54:37 +0200

    KRB-740 Legg til en respons

--- cypress/e2e/evaluation.cy.ts
+++ cypress/e2e/evaluation.cy.ts
@@ -16,6 +16,19 @@ describe('evaluation', () => {
     cy.contains('Last opp besvarelser');
     cy.contains('Manuell evaluering');
     cy.contains('Resultat');
+
+    cy.get('label')
+      .contains('Last opp besvarelser')
+      .selectFile('./cypress/filesForUploadTesting/2022-09-30_response-2.pdf');
+    cy.wait(1000);
+    cy.contains('regaergaerg'); // TODO: Create a better response file with a better filename : )
+    cy.contains('2022-09-30_response-2.pdf');
+
+    cy.contains('Manuell evaluering').click();
+
+    cy.contains('Evaluer besvarelser').click();
+
+    cy.contains('regaergaerg 93%').click();
   });
 
   it('fallback works when no specification is uploaded or found', () => {
