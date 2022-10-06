5b5178b14ab8e0cd7e8df7bbfa278d561d2ad1c0
RaghadAL-Abd <Raghad.Al-Abd@dfo.no>
2022-10-06 19:28:20 +0200

    krb-754 Ny redigeringside for produkt-kravspesifikasjon

--- src/pages/SpecEditor/EditProduct/ProductHeader/EditProduct.tsx
+++ src/pages/SpecEditor/EditProduct/ProductHeader/EditProduct.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Button, Typography } from '@mui/material';
 
 import DeleteSpecProduct from '../DeleteSpecProduct';
 import ProductHeader from './ProductHeader';
-import { Button } from '@mui/material';
 import { SPECIFICATION } from '../../../../common/PathConstants';
 import ProductNeed from './Elements/Needs/ProductNeed';
 import Utils from '../../../../common/Utils';
@@ -12,6 +14,9 @@ import { useSelectState } from '../../../Workbench/Create/SelectContext';
 import { useSpecificationState } from '../../SpecificationContext';
 import { IRouteSpecificationParams } from '../../../../models/IRouteSpecificationParams';
 import Panel from '../../../../components/UI/Panel/Panel';
+import EditProductForm from './EditProductForm';
+import Toolbar from '../../../../components/UI/Toolbar/ToolBar';
+import ToolbarItem from '../../../../components/UI/Toolbar/ToolbarItem';
 
 export default function EditProduct(): React.ReactElement {
   const { t } = useTranslation();
@@ -19,6 +24,7 @@ export default function EditProduct(): React.ReactElement {
   const { productId } = useParams<IRouteSpecificationParams>();
   const { specification } = useSpecificationState();
   const { setDeleteMode } = useSelectState();
+  const [editingProduct, setEditingProduct] = useState(false);
 
   const product = specification.products.find((prod) => prod.id === productId);
 
@@ -26,8 +32,30 @@ export default function EditProduct(): React.ReactElement {
     setDeleteMode('');
   };
   const toOverviewPage = (): void => {
-    history.push(`/${SPECIFICATION}/${specification.id}`);
+    if (!editingProduct) {
+      history.push(`/${SPECIFICATION}/${specification.id}`);
+    }
   };
+
+  const renderProductActionsToolbar = (): ReactElement => {
+    return (
+      <Toolbar gapType={'lg'} hasPadding={true}>
+        <ToolbarItem
+          primaryText={t('Edit product')}
+          icon={<EditIcon />}
+          handleClick={() => setEditingProduct(true)}
+        />
+        {product?.id && (
+          <ToolbarItem
+            primaryText={t('Delete product')}
+            icon={<DeleteIcon />}
+            handleClick={() => setDeleteMode(product?.id)}
+          />
+        )}
+      </Toolbar>
+    );
+  };
+
   const renderNeeds = () => {
     const needs = product
       ? Utils.findVariantsUsedByProduct(
@@ -43,17 +71,37 @@ export default function EditProduct(): React.ReactElement {
   return (
     <DeleteSpecProduct product={product} handleClose={onDelete}>
       <div>
-        <ProductHeader product={product} />
+        {!editingProduct && (
+          <Typography variant="lgBold">{product?.title}</Typography>
+        )}
+        {!editingProduct && renderProductActionsToolbar()}
+        <ProductHeader product={product} editingProduct={editingProduct} />
+        {product && editingProduct && (
+          <EditProductForm
+            handleClose={() => setEditingProduct(false)}
+            specificationProduct={product}
+          />
+        )}
         {renderNeeds()}
         <Panel
           sticky={true}
           panelColor={'white'}
           children={
             <>
-              <Button variant="cancel" onClick={toOverviewPage}>
+              <Button
+                variant="cancel"
+                onClick={toOverviewPage}
+                disabled={editingProduct}
+                sx={{ backgroundColor: editingProduct ? '#d1d1d1' : '' }}
+              >
                 {t('Cancel')}
               </Button>
-              <Button variant="primary" onClick={toOverviewPage}>
+              <Button
+                variant="primary"
+                onClick={toOverviewPage}
+                disabled={editingProduct}
+                sx={{ backgroundColor: editingProduct ? '#d1d1d1' : '' }}
+              >
                 {t('Save product')}
               </Button>
             </>
--- src/pages/SpecEditor/EditProduct/ProductHeader/ProductHeader.tsx
+++ src/pages/SpecEditor/EditProduct/ProductHeader/ProductHeader.tsx
@@ -1,12 +1,7 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { Typography } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
 import { useTranslation } from 'react-i18next';
 
-import DFODialog from '../../../../components/DFODialog/DFODialog';
-import EditProductForm from './EditProductForm';
-import { useSelectState } from '../../../Workbench/Create/SelectContext';
 import css from '../../../Stylesheets/EditorFullPage.module.scss';
 import { ISpecificationProduct } from '../../../../Nexus/entities/ISpecificationProduct';
 import Toolbar from '../../../../components/UI/Toolbar/ToolBar';
@@ -15,31 +10,14 @@ import { Weighting } from '../../../../Nexus/enums';
 
 interface IProps {
   product?: ISpecificationProduct;
+  editingProduct: boolean;
 }
 
-export default function ProductHeader({ product }: IProps): React.ReactElement {
+export default function ProductHeader({
+  product,
+  editingProduct
+}: IProps): React.ReactElement {
   const { t } = useTranslation();
-  const [editingProduct, setEditingProduct] = useState(false);
-  const { setDeleteMode } = useSelectState();
-
-  const renderProductActionsToolbar = (): ReactElement => {
-    return (
-      <Toolbar gapType={'lg'} hasPadding={true}>
-        <ToolbarItem
-          primaryText={t('Edit product')}
-          icon={<EditIcon />}
-          handleClick={() => setEditingProduct(true)}
-        />
-        {product?.id && (
-          <ToolbarItem
-            primaryText={t('Delete product')}
-            icon={<DeleteIcon />}
-            handleClick={() => setDeleteMode(product?.id)}
-          />
-        )}
-      </Toolbar>
-    );
-  };
 
   const renderProductInfoToolbar = (): ReactElement => {
     return (
@@ -66,25 +44,13 @@ export default function ProductHeader({ product }: IProps): React.ReactElement {
     <div className={css.overview}>
       {product ? (
         <>
-          <Typography variant="lgBold">{product?.title}</Typography>
-          {renderProductActionsToolbar()}
-          {renderProductInfoToolbar()}
-          <Typography variant="md">{product?.description}</Typography>
+          {!editingProduct && renderProductInfoToolbar()}
+          {!editingProduct && (
+            <Typography variant="sm">{product?.description}</Typography>
+          )}
         </>
       ) : (
         <Typography variant="lgBold">{t('General requirements')}</Typography>
-      )}
-      {product && editingProduct && (
-        <DFODialog
-          isOpen={true}
-          handleClose={() => setEditingProduct(false)}
-          children={
-            <EditProductForm
-              handleClose={() => setEditingProduct(false)}
-              specificationProduct={product}
-            />
-          }
-        />
       )}
     </div>
   );
