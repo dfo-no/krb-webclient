fdbba85ac01d3008d962486a4734c846abda37c7
RaghadAL-Abd <Raghad.Al-Abd@dfo.no>
2022-10-10 08:46:36 +0200

    krb-754 fix gap

--- src/components/Form/FormFieldsBox.tsx
+++ src/components/Form/FormFieldsBox.tsx
@@ -3,7 +3,7 @@ import { Box, styled } from '@mui/material';
 export const FormFieldsBox = styled(Box)(() => ({
   display: 'flex',
   flexDirection: 'column',
-  gap: `var(--small-gap)`,
+  gap: `var(--normal-gap)`,
   backgroundColor: `var(--white-color)`,
   border: `0.2rem var(--primary-color) solid`,
   padding: `var(--normal-gap)`
