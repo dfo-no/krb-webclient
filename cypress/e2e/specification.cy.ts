7fb6699968832d11cc36769d9918a9b35d94cba4
RaghadAL-Abd <Raghad.Al-Abd@dfo.no>
2022-10-20 12:07:45 +0200

    krb-778 endring etter merge develop and check formating

--- cypress/e2e/specification.cy.ts
+++ cypress/e2e/specification.cy.ts
@@ -1,29 +1,30 @@
 describe('Specification', () => {
-  it('can upload , edit and download specification', () => {
-    // Last opp spesifikasjon
+  const title = 'Test kravspesifikasjon';
+  const organisation = 'Itland Data';
+  const caseNumber = '123456/AA';
+  const currency = 'Norske kroner (NOK)';
+  const generalProduct = 'Generelle krav';
+  const productName = 'El-bil';
+  const addProductButton = 'Legg til produkt';
+  const editGeneralProductButton = 'Rediger generelle krav';
+  const editProductButton = 'Rediger produktet';
+  const deleteProductButton = 'Fjern produkt';
+
+  const newTitle = 'Test kravspesifikasjon 2';
+  const newOrganisation = 'dfø';
+  const newCaseNumber = '123456/BB';
+  const newCurrency = 'Europeiske euro (EUR)';
+
+  it('can upload and show specification', () => {
+    // upload specification
     cy.visit('localhost:3000');
     cy.contains('Last opp kravbank-fil').selectFile(
       './cypress/filesForUploadTesting/specification-1.pdf'
     );
     cy.contains('Rediger spesifikasjon').click();
+    cy.url().should('include', 'http://localhost:3000/specification/');
 
-    // Innhold i spesifikasjon
-    const title = 'Test spesifikasjon';
-    const organisation = 'Itland Data';
-    const caseNumber = '123456/AA';
-    const currency = 'Norske kroner (NOK)';
-    const generalProduct = 'Generelle krav';
-    const productName = 'Varebiler';
-    const addProductButton = 'Legg til produkt';
-    const editGeneralProductButton = 'Rediger generelle krav';
-    const editProductButton = 'Rediger produktet';
-    const deleteProductButton = 'Fjern produkt';
-
-    const newTitle = 'Test spesifikasjon 2';
-    const newOrganisation = 'dfø';
-    const newCaseNumber = '123456/BB';
-    const newCurrency = 'Europeiske euro (EUR)';
-
+    // Contents of specification
     cy.contains(title);
     cy.contains(organisation);
     cy.contains(caseNumber);
@@ -31,42 +32,64 @@ describe('Specification', () => {
     cy.contains(generalProduct);
     cy.contains(productName);
 
-    cy.get('div').contains(addProductButton).parent().should('have.attr', 'data-disabled', "false");
-    cy.get('div').contains(editGeneralProductButton).parent().should('have.attr', 'data-disabled', "false");
-    cy.get('div').contains(editProductButton).parent().should('have.attr', 'data-disabled', "false");
-    cy.get('div').contains(deleteProductButton).parent().should('have.attr', 'data-disabled', "false");
+    cy.get('div')
+      .contains(addProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'false');
+    cy.get('div')
+      .contains(editGeneralProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'false');
+    cy.get('div')
+      .contains(editProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'false');
+    cy.get('div')
+      .contains(deleteProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'false');
 
     // Liste av produkter
     cy.get('ul').find('li').should('have.length', 2);
+  });
 
-    // Rediger spesifikasjon
+  it('can edit specification', () => {
+    // edit specification
     cy.get('div').contains('Rediger spesifikasjon').click();
-    cy.get('input[placeholder="Navn på spesifikasjon"]').clear().type(
-      newTitle
-    );
+    cy.get('input[placeholder="Navn på spesifikasjon"]').clear().type(newTitle);
     cy.get('input[placeholder="Saksnummer"]').clear().type(newCaseNumber);
     cy.get('div').contains('Norske kroner (NOK)').click();
     cy.get('div').contains(newCurrency).click();
     cy.get('input[name="organization"]').clear().type(newOrganisation);
 
-    // Sjekk at alle andre handlingsknapper er deaktivert under redigering
-    cy.get('div').contains(addProductButton).parent().should('have.attr', 'data-disabled', "true");
-    cy.get('div').contains(editGeneralProductButton).parent().should('have.attr', 'data-disabled', "true");
-    cy.get('div').contains(editProductButton).parent().should('have.attr', 'data-disabled', "true");
-    cy.get('div').contains(deleteProductButton).parent().should('have.attr', 'data-disabled', "true");
+    // check that all other action buttons are disabled while editing
+    cy.get('div')
+      .contains(addProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'true');
+    cy.get('div')
+      .contains(editGeneralProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'true');
+    cy.get('div')
+      .contains(editProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'true');
+    cy.get('div')
+      .contains(deleteProductButton)
+      .parent()
+      .should('have.attr', 'data-disabled', 'true');
 
-    // lagre
+    // save changes
     cy.get('button').contains('Lagre').click();
 
-    // Ny innhold i spesifikasjon etter redigering
+    // New content in specification after editing
     cy.contains(newTitle);
     cy.contains(newOrganisation);
     cy.contains(newCaseNumber);
     cy.contains(newCurrency);
 
-    // lagre spesifikasjon
+    // save specification
     cy.get('div').contains('Last ned spesifikasjon').click();
-
-
   });
 });
