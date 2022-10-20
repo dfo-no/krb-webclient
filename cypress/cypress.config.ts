3ebb730f65b9f9f59faae9188d736f8bf75c9ec4
erik-itland <evi@computas.com>
2022-10-20 08:17:55 +0200

    KRB-780 Fix cypress files

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    }
-  }
+    },
+  },
 });
