9c85b4891868d6530fcef7cb43a05324aa10e56b
RaghadAL-Abd <Raghad.Al-Abd@dfo.no>
2022-10-21 11:49:41 +0200

    krb-786 Første forsøk å bruke cypress-cucumber-preprocessor

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'cypress';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import browserify from '@badeball/cypress-cucumber-preprocessor/browserify';
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    'file:preprocessor',
+    browserify(config, {
+      typescript: require.resolve('typescript'),
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 export default defineConfig({
   e2e: {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: 'cypress/e2e/*.*',
+    setupNodeEvents,
     supportFile: 'cypress/support/e2e.ts',
   },
 });
