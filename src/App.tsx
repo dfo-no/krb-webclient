590ada4c6c7d8607c09356d9cda700f138c142a5
erik-itland <112852201+erik-itland@users.noreply.github.com>
2022-11-03 20:57:36 +0100

    KRB-763 Add Application Insights (#631)
    * KRB-763 Add Application Insights
    
    * KRB-763 Trekk ut connection string for Application Insights
    
    * KRB-763 Bruk samme connectionstring for DEV og PROD
    
    * KRB-763 Legge til tracking av environment
    
    * KRB-763 Oppdatere README med REACT_APP_APPLICATION_INSIGHTS_ENVIRONMENT
    
    Co-authored-by: Erik Vårdal Itland <erik.vardal.itland@entur.org>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import { CssBaseline } from '@mui/material';
 import { MsalProvider } from '@azure/msal-react';
 import { PublicClientApplication } from '@azure/msal-browser';
 import { Route, Switch, useLocation } from 'react-router-dom';
+import {
+  ApplicationInsights,
+  ITelemetryItem,
+} from '@microsoft/applicationinsights-web';
+import {
+  AppInsightsContext,
+  ReactPlugin,
+} from '@microsoft/applicationinsights-react-js';
+import { createBrowserHistory } from 'history';
 
 import AlertList from './components/Alert/AlertList';
 import EvaluationModule from './pages/Evaluation/EvaluationModule';
@@ -18,6 +27,37 @@ import { msalConfig } from './authentication/authConfig';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
 import Footer from './Footer/Footer';
 
+const browserHistory = createBrowserHistory();
+const reactPlugin = new ReactPlugin();
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString:
+      process.env.REACT_APP_APPLICATION_INSIGHTS_CONNECTION_STRING,
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory },
+    },
+    disableFetchTracking: true,
+  },
+});
+appInsights.loadAppInsights();
+
+appInsights.addTelemetryInitializer((env: ITelemetryItem) => {
+  // eslint-disable-next-line no-param-reassign
+  env.tags = env.tags || [];
+
+  // Default is krb-webclient. I don't know where Application Insights get it from, so I'll leave this here as an
+  // example for now.
+
+  // eslint-disable-next-line no-param-reassign
+  // env.tags['ai.cloud.role'] = 'krb-webclient';
+
+  // eslint-disable-next-line no-param-reassign
+  env.data = env.data || [];
+  // eslint-disable-next-line no-param-reassign
+  env.data.environment = process.env.REACT_APP_APPLICATION_INSIGHTS_ENVIRONMENT;
+});
+
 const msalInstance = new PublicClientApplication(msalConfig);
 
 function App(): ReactElement {
@@ -27,16 +67,21 @@ function App(): ReactElement {
 
   function renderContent(): ReactElement {
     return (
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/workbench" component={WorkbenchModule} />
-        <Route path="/specification" component={SpecModule} />
-        <Route path="/response" component={ResponseModule} />
-        <Route path="/evaluation" component={EvaluationModule} />
-        <Route path="/prefilledresponse" component={PrefilledResponseModule} />
-      </Switch>
+      <AppInsightsContext.Provider value={reactPlugin}>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/workbench" component={WorkbenchModule} />
+          <Route path="/specification" component={SpecModule} />
+          <Route path="/response" component={ResponseModule} />
+          <Route path="/evaluation" component={EvaluationModule} />
+          <Route
+            path="/prefilledresponse"
+            component={PrefilledResponseModule}
+          />
+        </Switch>
+      </AppInsightsContext.Provider>
     );
   }
 
