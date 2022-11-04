d0f709fdfe94271e3b5a6cc1d4ec6d12a264c5b5
Erik Vårdal Itland <erik.vardal.itland@entur.org>
2022-11-04 19:38:06 +0100

    KRB-805 Oppdatere header i besvarelses-side

--- src/pages/Response/Answer/ProductHeader.tsx
+++ src/pages/Response/Answer/ProductHeader.tsx
@@ -2,46 +2,36 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
-import css from '../../Stylesheets/EditorFullPage.module.scss';
-import { DFOCardHeader } from '../../../components/DFOCard/DFOCardHeader';
-import { DFOHeaderContentBox } from '../../../components/DFOCard/DFOHeaderContentBox';
 import { useAppSelector } from '../../../store/hooks';
 import { useProductIndexState } from '../../../components/ProductIndexContext/ProductIndexContext';
+import ToolbarItem from '../../../components/UI/Toolbar/ToolbarItem';
+import Toolbar from '../../../components/UI/Toolbar/ToolBar';
 
 export default function ProductHeader(): React.ReactElement {
   const { t } = useTranslation();
   const { response } = useAppSelector((state) => state.response);
   const { productIndex } = useProductIndexState();
 
+  const description =
+    response.specification.products[productIndex]?.description;
+  const originProduct =
+    response.specification.products[productIndex].originProduct;
   return (
-    <DFOCardHeader>
-      <DFOHeaderContentBox>
-        <div className={css.HeaderTop}>
-          <Typography variant="lgBold">
-            {response.specification.products[productIndex]?.title ??
-              t('General requirement')}
-          </Typography>
-        </div>
-        <div className={css.HeaderBottom}>
-          <Typography variant="smBold">
-            {response.specification.products[productIndex]?.description ?? ''}
-          </Typography>
+    <>
+      <Typography variant="lgBold">
+        {response.specification.products[productIndex]?.title ??
+          t('General requirement')}
+      </Typography>
+      <Toolbar gapType={'md'}>
+        {description && <ToolbarItem primaryText={description} />}
 
-          {productIndex !== -1 &&
-            response.specification.products[productIndex].originProduct && (
-              <Typography className={css.ProductType} variant="smBold">
-                {t('From product type')}
-                {': '}
-                <i>
-                  {
-                    response.specification.products[productIndex].originProduct
-                      .title
-                  }
-                </i>
-              </Typography>
-            )}
-        </div>
-      </DFOHeaderContentBox>
-    </DFOCardHeader>
+        {productIndex !== -1 && originProduct && (
+          <ToolbarItem
+            primaryText={t('From product type')}
+            secondaryText={originProduct.title}
+          />
+        )}
+      </Toolbar>
+    </>
   );
 }
