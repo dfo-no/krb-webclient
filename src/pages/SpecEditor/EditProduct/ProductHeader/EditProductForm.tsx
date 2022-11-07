0e003dfd3c9cad2952b56b18deeef1b2a7c7c822
RaghadAL-Abd <Raghad.Al-Abd@dfo.no>
2022-11-07 11:04:46 +0100

    krb-806 I kravsspesifikasjon - fjern vekting alle steder, inkludert der det er kommentert ut

--- src/pages/SpecEditor/EditProduct/ProductHeader/EditProductForm.tsx
+++ src/pages/SpecEditor/EditProduct/ProductHeader/EditProductForm.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Typography from '@mui/material/Typography';
-import { FormProvider, useForm, useWatch } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { Box } from '@mui/material';
 
 import Nexus from '../../../../Nexus/Nexus';
 import VerticalTextCtrl from '../../../../FormProvider/VerticalTextCtrl';
@@ -12,10 +11,8 @@ import {
   ModalButtonsBox,
 } from '../../../../components/ModalBox/ModalBox';
 import { useSpecificationState } from '../../SpecificationContext';
-import { ModelType, Weighting, WeightingStep } from '../../../../Nexus/enums';
+import { ModelType } from '../../../../Nexus/enums';
 import css from './ProductHeader.module.scss';
-import SliderCtrl from '../../../../FormProvider/SliderCtrl';
-import { IMark } from '../../../../Nexus/entities/IMark';
 import { FormFieldsBox } from '../../../../components/Form/FormFieldsBox';
 
 interface IProps {
@@ -27,20 +24,11 @@ const EditProductForm = ({ handleClose, specificationProduct }: IProps) => {
   const { t } = useTranslation();
   const { editSpecificationProduct } = useSpecificationState();
   const nexus = Nexus.getInstance();
-  const [sliderMark, setSliderMark] = useState<IMark[]>([
-    { value: Weighting.MEDIUM, label: t(Weighting[Weighting.MEDIUM]) },
-  ]);
 
   const methods = useForm<ISpecificationProduct>({
     resolver: nexus.resolverService.resolver(ModelType.specificationProduct),
     defaultValues: specificationProduct,
   });
-
-  const useWeight = useWatch({ name: 'weight', control: methods.control });
-
-  useEffect(() => {
-    setSliderMark([{ value: useWeight, label: t(Weighting[useWeight]) }]);
-  }, [t, useWeight]);
 
   const onSubmit = (put: ISpecificationProduct): void => {
     editSpecificationProduct(put);
@@ -82,21 +70,6 @@ const EditProductForm = ({ handleClose, specificationProduct }: IProps) => {
               </Typography>
             }
           />
-          <Box className={css.SlideBoxWrapper}>
-            <Typography variant={'smBold'} sx={{ marginBottom: 1 }}>
-              {t('Weighting')}
-            </Typography>
-            <Box className={css.SlideBox}>
-              <SliderCtrl
-                name={'weight'}
-                min={Weighting.LOWEST}
-                step={WeightingStep}
-                max={Weighting.HIGHEST}
-                showValue={false}
-                marks={sliderMark}
-              />
-            </Box>
-          </Box>
           <ModalButtonsBox>
             <ModalButton variant="cancel" onClick={() => handleClose()}>
               {t('common.Cancel')}
