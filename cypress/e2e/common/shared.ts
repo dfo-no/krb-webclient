780c9217be07b32fcc6ba7de1ccff604bb0b3a11
Haroon Alam <haroon.alam@dfo.no>
2022-11-08 10:45:21 +0100

    KRB-733: cypress test fiks

--- cypress/e2e/common/shared.ts
+++ cypress/e2e/common/shared.ts
@@ -32,6 +32,10 @@ Then('Jeg klikker på {string} knappen', (button: string) => {
   cy.get('button').contains(button).click();
 });
 
+When('Jeg kan velge navn ved å klikke på {string} knappen', (button: string) => {
+  cy.get('li > div > button').contains(button).click();
+});
+
 When(
   'Jeg skriver {string} i feltet {string}',
   (text: string, fieldName: string) => {
