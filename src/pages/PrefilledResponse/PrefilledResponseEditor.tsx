1f966d1ac11bb48400ac740a5bc4a090cc37c2a8
RaghadAL-Abd <Raghad.Al-Abd@dfo.no>
2022-11-11 10:19:49 +0100

    krb-819 Ny design for forberedt besvarelse-oversikt side

--- src/pages/PrefilledResponse/PrefilledResponseEditor.tsx
+++ src/pages/PrefilledResponse/PrefilledResponseEditor.tsx
@@ -1,29 +1,53 @@
 import React, { ReactElement } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import css from '../Stylesheets/Editor.module.scss';
-import PrefilledResponseSidebar from './SideBar/PrefilledResponseSidebar';
+import css from '../Stylesheets/EditorFullPage.module.scss';
 import AnswerProduct from './Answer/AnswerProduct';
 import NewProduct from './NewProduct/NewProduct';
 import NoProducts from './NoProducts/NoProducts';
 import { useProductIndexState } from '../../components/ProductIndexContext/ProductIndexContext';
+import PrefilledResponseOverview from './PrefilledResponseOverview/PrefilledResponseOverview';
+import { PRODUCTS, PREFILLED_RESPONSE } from '../../common/PathConstants';
+import { SelectProvider } from '../Workbench/Create/SelectContext';
+import { useAppSelector } from '../../store/hooks';
 
-export default function PrefilledResponseEditor(): React.ReactElement {
+export default function PrefilledResponseEditor(): ReactElement {
+  const { prefilledResponse } = useAppSelector(
+    (state) => state.prefilledResponse
+  );
   const { create, productIndex } = useProductIndexState();
 
-  const renderProduct = (): ReactElement => {
+  const renderProduct = () => {
     if (create) {
       return <NewProduct />;
     }
     if (productIndex >= -1) {
       return <AnswerProduct />;
     }
-    return <NoProducts />;
+    if (prefilledResponse.products.length == 0) {
+      return <NoProducts />;
+    }
   };
 
   return (
-    <div className={css.Editor}>
-      <PrefilledResponseSidebar />
-      <div className={css.Content} children={renderProduct()} />
+    <div className={css.EditorFullPage}>
+      <Switch>
+        <Route
+          exact
+          path={`/${PREFILLED_RESPONSE}/${prefilledResponse.bank.id}`}
+        >
+          <SelectProvider>
+            <PrefilledResponseOverview />
+          </SelectProvider>
+        </Route>
+        <Route
+          path={`/${PREFILLED_RESPONSE}/${prefilledResponse.bank.id}/${PRODUCTS}/`}
+        >
+          <SelectProvider>
+            <div className={css.Content} children={renderProduct()} />
+          </SelectProvider>
+        </Route>
+      </Switch>
     </div>
   );
 }
