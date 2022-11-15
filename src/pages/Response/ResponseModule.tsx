2e3fd258c32f67934c9f75578ae9cd522278b9d9
Erik Vårdal Itland <evi@computas.com>
2022-11-15 18:58:56 +0100

    KRB-808 fjerne unødvendig innpakning

--- src/pages/Response/ResponseModule.tsx
+++ src/pages/Response/ResponseModule.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { Route, RouteComponentProps, Switch } from 'react-router-dom';
 
-import ResponseEditor from './ResponseEditor';
+import AnswerProduct from './Answer/AnswerProduct';
+import css from '../Stylesheets/EditorFullPage.module.scss';
+import ResponseOverview from './Overview/ResponseOverview';
+import { PRODUCTS } from '../../common/PathConstants';
 import { ResponseProvider } from './ResponseContext';
 
 export type MatchParams = { responseId: string };
@@ -12,7 +15,19 @@ export default function ResponseModule({
 }: Props): React.ReactElement {
   return (
     <ResponseProvider {...props}>
-      <ResponseEditor {...props} />
+      <div className={css.EditorFullPage}>
+        <div className={css.Content}>
+          <Switch>
+            <Route exact path={`${props.match.path}`}>
+              <ResponseOverview />
+            </Route>
+            <Route
+              path={`${props.match.path}/${PRODUCTS}/:productIndex`}
+              component={AnswerProduct}
+            />
+          </Switch>
+        </div>
+      </div>
     </ResponseProvider>
   );
 }
