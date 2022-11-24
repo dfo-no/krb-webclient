a9da751cf148e6981c24e32fa3f565272c6ac5e6
haroonalam16 <91733674+haroonalam16@users.noreply.github.com>
2022-11-24 09:24:21 +0100

    KRB-822: Forberedt besvarelse : Redigere produkt og legg til relatert… (#656)
    * KRB-822: Forberedt besvarelse : Redigere produkt og legg til relaterte produkter
    
    * KRB-822: Fiks for relaterte produkter
    
    * KRB-822: Refactor EditProductForm komponent
    
    * KRB-822: Skjul Rediger produktdetaljer-knapp når du besvarte

---
+++ src/pages/PrefilledResponse/EditProduct/EditProductForm.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import { FormProvider, useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { FormControlLabel, RadioGroup } from '@mui/material';
+
+import Nexus from '../../../Nexus/Nexus';
+import VerticalTextCtrl from '../../../FormProvider/VerticalTextCtrl';
+import {
+  ModalButton,
+  ModalButtonsBox,
+} from '../../../components/ModalBox/ModalBox';
+import { ModelType } from '../../../Nexus/enums';
+import { FormFieldsBox } from '../../../components/Form/FormFieldsBox';
+import { IPrefilledResponseProduct } from '../../../Nexus/entities/IPrefilledResponseProduct';
+import { editProduct } from '../../../store/reducers/prefilled-response-reducer';
+import { DFORadio } from '../../../components/DFORadio/DFORadio';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import { useProductIndexState } from '../../../components/ProductIndexContext/ProductIndexContext';
+import MultipleProductSelection from '../../../components/ProductSelection/MultipleProductSelection';
+import { Parentable } from '../../../models/Parentable';
+import { IProduct } from '../../../Nexus/entities/IProduct';
+
+interface IProps {
+  handleClose: () => void;
+  prefilledResponseProduct: IPrefilledResponseProduct;
+}
+
+const EditProductForm = ({ handleClose, prefilledResponseProduct }: IProps) => {
+  const { t } = useTranslation();
+  const nexus = Nexus.getInstance();
+  const { prefilledResponse } = useAppSelector(
+    (state) => state.prefilledResponse
+  );
+  const { productIndex } = useProductIndexState();
+  const dispatch = useAppDispatch();
+  const [relatedProducts, setRelatedProducts] = useState(false);
+  const options = [
+    { value: true, label: t('common.Yes'), recommended: false },
+    { value: false, label: t('common.No'), recommended: false },
+  ];
+  const methods = useForm<IPrefilledResponseProduct>({
+    resolver: nexus.resolverService.resolver(
+      ModelType.prefilledResponseProduct
+    ),
+    defaultValues: prefilledResponseProduct,
+  });
+
+  const onSubmit = (put: IPrefilledResponseProduct): void => {
+    dispatch(editProduct({ product: put, productIndex: productIndex }));
+    handleClose();
+  };
+
+  const relatedProductsClicked = () => {
+    if (relatedProducts) {
+      methods.setValue('relatedProducts', []);
+    }
+    setRelatedProducts(!relatedProducts);
+  };
+  const nonDeletedRelatedProducts: Parentable<IProduct>[] =
+    prefilledResponse.bank.products.filter((item) => !item.deletedDate);
+
+  return (
+    <FormProvider {...methods}>
+      <form
+        onSubmit={methods.handleSubmit(onSubmit)}
+        autoComplete="off"
+        noValidate
+      >
+        <FormFieldsBox>
+          <Typography variant="lgBold">{t('Edit product details')}</Typography>
+          <VerticalTextCtrl
+            name="title"
+            label={t('Name of product')}
+            placeholder={t('Name of product')}
+            autoFocus
+          />
+          <VerticalTextCtrl
+            name="description"
+            label={t('Description of the product')}
+            placeholder={t('Description of the product')}
+          />
+          <Typography variant={'smBold'}>
+            {t('Add additional related products')}
+          </Typography>
+          <RadioGroup
+            row={true}
+            value={options[relatedProducts ? 0 : 1].value}
+            onClick={relatedProductsClicked}
+          >
+            {options.map((option) => {
+              return (
+                <FormControlLabel
+                  key={'' + option.value}
+                  value={option.value}
+                  control={<DFORadio />}
+                  label={<Typography variant={'sm'}>{option.label}</Typography>}
+                />
+              );
+            })}
+          </RadioGroup>
+          {nonDeletedRelatedProducts.length > 0 && relatedProducts && (
+            <MultipleProductSelection
+              name={'relatedProducts'}
+              products={nonDeletedRelatedProducts}
+            />
+          )}
+          <ModalButtonsBox>
+            <ModalButton variant="cancel" onClick={() => handleClose()}>
+              {t('common.Cancel')}
+            </ModalButton>
+            <ModalButton variant="primary" type="submit">
+              {t('Save')}
+            </ModalButton>
+          </ModalButtonsBox>
+        </FormFieldsBox>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default EditProductForm;
