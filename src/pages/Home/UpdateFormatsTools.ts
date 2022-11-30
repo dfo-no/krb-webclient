89db164b107500a4fcd5c3614e5423bc379cc0f6
Erik Vårdal Itland <evi@computas.com>
2022-11-30 12:57:48 +0100

    KRB-842 Fiks alle filformat

---
+++ src/pages/Home/UpdateFormatsTools.ts
@@ -0,0 +1,60 @@
+import produce from 'immer';
+
+export const updateObject = <T extends Record<string, unknown>>(
+  unwrappedObject: T
+) => {
+  return produce(unwrappedObject, (draft) => {
+    type Input = Record<string, unknown>;
+
+    function isRecord(value: unknown): value is Record<string, unknown> {
+      return typeof value === 'object' && value !== null;
+    }
+
+    const translate = (input: Input) => {
+      for (const key of Object.keys(input)) {
+        if (key.toLowerCase().includes('weight')) {
+          // eslint-disable-next-line no-param-reassign
+          delete input[key];
+        }
+
+        if (key.includes('points')) {
+          const keyParts = key.split('points');
+          const translatedKey = keyParts[0] + 'discount' + keyParts[1];
+          // eslint-disable-next-line no-param-reassign
+          input[translatedKey] = input[key];
+          // eslint-disable-next-line no-param-reassign
+          delete input[key];
+        } else if (key.includes('point')) {
+          const keyParts = key.split('point');
+          const translatedKey = keyParts[0] + 'discount' + keyParts[1];
+          // eslint-disable-next-line no-param-reassign
+          input[translatedKey] = input[key];
+          // eslint-disable-next-line no-param-reassign
+          delete input[key];
+        }
+        if (key.includes('Points')) {
+          const keyParts = key.split('Points');
+          const translatedKey = keyParts[0] + 'Discount' + keyParts[1];
+          // eslint-disable-next-line no-param-reassign
+          input[translatedKey] = input[key];
+          // eslint-disable-next-line no-param-reassign
+          delete input[key];
+        } else if (key.includes('Point')) {
+          const keyParts = key.split('Point');
+          const translatedKey = keyParts[0] + 'Discount' + keyParts[1];
+          // eslint-disable-next-line no-param-reassign
+          input[translatedKey] = input[key];
+          // eslint-disable-next-line no-param-reassign
+          delete input[key];
+        }
+
+        const toCheck = input[key];
+
+        if (isRecord(toCheck)) {
+          translate(toCheck);
+        }
+      }
+    };
+    translate(draft);
+  });
+};
