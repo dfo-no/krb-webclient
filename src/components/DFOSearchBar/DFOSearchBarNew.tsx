3845b7708aa6f51ed19e9c3007ba8c17eac200a0
Erik Vårdal Itland <evi@computas.com>
2022-12-21 09:31:51 +0100

    KRB-858 Fikse søk

---
+++ src/components/DFOSearchBar/DFOSearchBarNew.tsx
@@ -0,0 +1,61 @@
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import makeStyles from '@mui/styles/makeStyles';
+import React from 'react';
+
+import DFOInput from '../DFOTextField/DFOTextField';
+import theme from '../../theme';
+
+// TODO: Move to where other entities are
+export type TitleAndDescription = {
+  title?: string;
+  description?: string;
+};
+interface DFOSearchBarProps<T extends TitleAndDescription> {
+  list: T[];
+  searchFunction: (searchString: string, list: T[]) => T[];
+  callback: (list: T[]) => void;
+  placeholder: string;
+}
+
+const useStyles = makeStyles({
+  searchFieldIcon: {
+    marginBottom: 2,
+    marginRight: 10,
+    color: theme.palette.secondary.main,
+    zIndex: 1,
+  },
+  adornedEnd: {
+    backgroundColor: theme.palette.white.main,
+  },
+});
+
+export function DFOSearchBar<T extends TitleAndDescription>({
+  list,
+  callback,
+  searchFunction,
+  placeholder,
+}: DFOSearchBarProps<T>): React.ReactElement {
+  const classes = useStyles();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.value.length !== 0) {
+      callback(searchFunction(e.target.value, list));
+    } else {
+      callback(list);
+    }
+  };
+
+  return (
+    <DFOInput
+      placeholder={placeholder}
+      onChange={onChange}
+      endAdornment={
+        <InputAdornment position="end">
+          <SearchIcon className={classes.searchFieldIcon} />
+        </InputAdornment>
+      }
+      disableUnderline
+    />
+  );
+}
