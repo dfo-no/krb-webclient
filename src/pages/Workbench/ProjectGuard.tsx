10615f12f555d20fa43d7da66dbf69476cb158ce
Erik Vårdal Itland <evi@computas.com>
2022-12-22 09:40:11 +0100

    KRB-858 "Fot i bakken"

--- src/pages/Workbench/ProjectGuard.tsx
+++ src/pages/Workbench/ProjectGuard.tsx
@@ -1,5 +1,4 @@
 import { Route, useParams } from 'react-router-dom';
-import { Fetcher } from 'openapi-typescript-fetch';
 
 import AdminGuard from './Admin/AdminGuard';
 import Create from './Create/Create';
@@ -9,32 +8,16 @@ import ProjectNotFound from '../../components/ProjectNotFound/ProjectNotFound';
 import { PreviewProvider } from './Preview/PreviewContext';
 import { SelectProvider } from './Create/SelectContext';
 import { useGetProjectQuery } from '../../store/api/bankApi';
-import { paths } from './../../api/generated';
 import { WORKBENCH } from '../../common/PathConstants';
 
 interface RouteParams {
   projectId: string;
 }
 
+// TODO: What about this name?
 export default function ProjectGuard(): React.ReactElement {
   const { projectId } = useParams<RouteParams>();
   const { data: project, isLoading } = useGetProjectQuery(projectId);
-
-  // declare fetcher for paths
-  const fetcher = Fetcher.for<paths>();
-
-  fetcher.configure({
-    baseUrl: 'https://krb-backend-api.azurewebsites.net',
-    // init: {
-    //   headers: {
-    //     ...
-    // },
-    // },
-    // use: [...] // middlewares
-  });
-
-  const findProjects = fetcher.path('/api/v1/projects').method('get').create();
-  findProjects({}).then((projects) => console.log(projects));
 
   if (isLoading) {
     return <LoaderSpinner />;
