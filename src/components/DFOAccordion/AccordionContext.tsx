6c8c1e13fa988685d9c93294946589fdc2d7dc25
Erik Vårdal Itland <evi@computas.com>
2023-01-23 11:01:18 +0100

    KRB-906 Forenkle flere context-er
    (cherry picked from commit 31c43751cc69a21167751d6a7bd00a36a7d0e426)

--- src/components/DFOAccordion/AccordionContext.tsx
+++ src/components/DFOAccordion/AccordionContext.tsx
@@ -1,52 +1,15 @@
-import {
-  createContext,
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useContext,
-  useState,
-} from 'react';
+import { useState } from 'react';
+import { createContainer } from 'unstated-next';
 
-interface IAccordionContext {
-  activeKey: string;
-  setActiveKey: Dispatch<SetStateAction<string>>;
-}
-
-const initialContext: IAccordionContext = {
-  activeKey: '',
-  setActiveKey: function (): void {
-    throw new Error('Function not implemented.');
-  },
-};
-
-const AccordionContext = createContext<IAccordionContext>(initialContext);
-
-interface IProps {
-  children: ReactNode;
-}
-
-export const AccordionProvider = ({ children }: IProps) => {
+const useAccordionContext = () => {
   const [activeKey, setActiveKey] = useState('');
 
-  return (
-    <AccordionContext.Provider
-      value={{
-        activeKey,
-        setActiveKey,
-      }}
-    >
-      {children}
-    </AccordionContext.Provider>
-  );
+  return {
+    activeKey,
+    setActiveKey,
+  };
 };
 
-export const useAccordionState = (): IAccordionContext => {
-  const context = useContext(AccordionContext);
-
-  if (context === undefined) {
-    throw new Error(
-      'useAccordionState must be used within a AccordionProvider'
-    );
-  }
-  return context;
-};
+export const AccordionContextContainer = createContainer(useAccordionContext);
+export const useAccordionState = AccordionContextContainer.useContainer;
+export const AccordionProvider = AccordionContextContainer.Provider;
